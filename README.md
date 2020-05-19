43485a0fbd4d26f293a233befda5ace276a323cf
Abhinav Rastogi <abhinav.rastogi@harness.io>
2020-05-19 09:18:06 +0530

    feat: [CDNG-11]: add blueprint and uikit

--- README.md
+++ README.md
@@ -9,14 +9,6 @@ $ yarn dev
 
 This will start the local server in watch mode with hot reloading.
 
-For login, you need to run `wingsui` repo first. Once logged in, come back here and everything should work.
-<details>
-  <summary>Details</summary>
-  Login and credential management is not implemented in `nextgenui` yet. When you login in `wingsui`, your auth tokens are set against `localhost:8181`, which can be read by this server since it is running on the same port.
-  
-  You can also use `nginx` on your machine to run both `wingsui` and `nextgenui` simultaneously if needed.
-</details>
-
 ### Publishing
 
 ```
@@ -25,19 +17,4 @@ $ yarn docker <tagname>
 ```
 
 First command will create a production build (minified, optimised).
-
 Second command will create a docker image and _publish_ it to `harness/nextgenui` Dockerhub repo.
-
-
-### Utilities
-
-Run lint checks
-```
-$ yarn lint
-```
-
-
-Run unit tests
-```
-$ yarn test
-```
--- src/modules/common/pages/Dashboard/Dashboard.tsx
+++ src/modules/common/pages/Dashboard/Dashboard.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
+import { Layout, Button } from '@wings-software/uikit';
 
 import { getUsers } from 'modules/common/services/UserService';
 
-import css from './Dashboard.css';
+import css from './Dashboard.module.scss';
 
 const Dashboard: React.FC = () => {
   const { accountId } = useParams();
@@ -15,7 +16,12 @@ const Dashboard: React.FC = () => {
   return (
     <div className={css.main}>
       <h1>Dashboard</h1>
-      <Link to="/pipeline-studio">Pipeline Studio</Link>
+      <Link to="/pipeline-studio">Go to Pipeline Studio</Link>
+      <br />
+      <Layout.Horizontal spacing="small" id="primary-buttons">
+        <Button intent="success" text="Test Button" onClick={() => alert('Hello World')} />
+        <Button intent="primary" icon="plus" onClick={() => alert('Hello World')} />
+      </Layout.Horizontal>
     </div>
   );
 };
