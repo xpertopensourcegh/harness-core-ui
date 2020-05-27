e5eb7fa7d0b622c8718a09adb6fa5a2290e6b4bb
Tan Nhu <tan@harness.io>
2020-05-27 00:59:26 -0700

    Rename useAppState -> useAppStore

--- src/framework/hooks/useAppStore.ts
+++ src/framework/hooks/useAppStore.ts
@@ -2,7 +2,7 @@ import { useState, useCallback } from 'react'
 import type { AppStore } from 'framework'
 import constate from 'constate'
 
-function useAppState(): { store: AppStore; updateStore: (newState: Partial<AppStore>) => void } {
+function useAppStore(): { store: AppStore; updateStore: (newState: Partial<AppStore>) => void } {
   const [store, setStore] = useState({} as AppStore)
   const updateStore = useCallback(
     (newState: Partial<AppStore>) => setStore(currentState => ({ ...currentState, ...newState })),
@@ -13,7 +13,7 @@ function useAppState(): { store: AppStore; updateStore: (newState: Partial<AppSt
 }
 
 const [AppStoreProvider, useAppStoreReader, useAppStoreWriter] = constate(
-  useAppState,
+  useAppStore,
   value => value.store,
   value => value.updateStore
 )
