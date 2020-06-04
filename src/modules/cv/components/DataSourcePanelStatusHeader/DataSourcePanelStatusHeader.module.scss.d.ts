9903108e8991b2969352d571a998248f8c40ac95
anjanBalgovind <anjan.balgovind@harness.io>
2020-06-03 20:05:04 -0700

    feat: [CVNG-39]: data source page panel status header

---
+++ src/modules/cv/components/DataSourcePanelStatusHeader/DataSourcePanelStatusHeader.module.scss.d.ts
@@ -0,0 +1,9 @@
+/* eslint-disable */
+// this is an auto-generated file
+declare const styles: {
+  readonly main: string
+  readonly tag: string
+  readonly tagError: string
+  readonly tagSuccess: string
+}
+export default styles
