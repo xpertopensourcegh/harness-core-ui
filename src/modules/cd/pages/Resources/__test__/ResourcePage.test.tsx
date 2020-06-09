b9445e24345c24add488653c24d81e07836b477a
Madhusudan K <madhusudan.khoradiya@harness.io>
2020-06-08 17:10:10 -0700

    fix: [CDNG-81]: Add test coverage and export component

--- src/modules/cd/pages/Resources/__test__/ResourcePage.test.tsx
+++ src/modules/cd/pages/Resources/__test__/ResourcePage.test.tsx
@@ -3,12 +3,10 @@ import React from 'react'
 import { render, queryByText } from '@testing-library/react'
 import i18n from '../ResourcesPage.i18n'
 import ResourcesPage from '../ResourcesPage'
-import moment from 'moment-timezone'
 
 jest.spyOn(Date, 'now').mockImplementation(() => 1588885751000)
 
 describe('ResourcesPage Snapshot', () => {
-  moment.tz.setDefault('PST')
   test('should render ResourcesPage', () => {
     const { container } = render(<ResourcesPage />)
     expect(queryByText(container, i18n.title)).toBeDefined()
--- src/modules/common/components/CustomTable/__test__/CustomTable.test.tsx
+++ src/modules/common/components/CustomTable/__test__/CustomTable.test.tsx
@@ -2,11 +2,9 @@ import React from 'react'
 import { render } from '@testing-library/react'
 import { data, columns } from './SampleColumnsData'
 import CustomTable from '../CustomTable'
-import moment from 'moment-timezone'
 
 jest.spyOn(Date, 'now').mockImplementation(() => 1588885751000)
 describe('CustomTable Snapshot', () => {
-  moment.tz.setDefault('PST')
   test('should render CustomTable with Proper Data and Columns', () => {
     const { container } = render(<CustomTable data={data} columns={columns} />)
     expect(container).toMatchSnapshot()
