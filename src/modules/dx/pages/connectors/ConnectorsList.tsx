21c5ac822a9720d1b36cc397a65cc68d57170784
Meenakshi Raikwar <meenakshi.raikwar@harness.io>
2020-06-15 15:02:40 +0530

    feat: [CDNG-127]:  Moved css to css file

--- src/modules/dx/pages/connectors/ConnectorsList.tsx
+++ src/modules/dx/pages/connectors/ConnectorsList.tsx
@@ -3,21 +3,13 @@ import { Layout, Container } from '@wings-software/uikit'
 import { DelegateSetupModal } from '../../../cd/modals/DelegateSetupModal/DelegateSetupModal'
 import CustomTable from '../../../common/components/CustomTable/CustomTable'
 import { data as rowData, columns } from '../../../cd/pages/Resources/SampleColumnsData'
+import css from './ConnectorsList.module.scss'
 
 const ConnectorsList = () => {
   return (
     <Layout.Vertical style={{ background: 'var(--grey-100)' }}>
       <Container>
-        <Layout.Horizontal
-          id="layout-horizontal-sample"
-          spacing="none"
-          padding="xlarge"
-          style={{
-            borderTop: '1px solid var(--grey-200)',
-            borderBottom: '1px solid var(--grey-200)',
-            background: 'white'
-          }}
-        >
+        <Layout.Horizontal id="layout-horizontal-sample" spacing="none" padding="xlarge" className={css.listWrapper}>
           <div style={{ width: 200 }}>
             <DelegateSetupModal />
           </div>
