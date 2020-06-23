87f698f53a6183b63229d12cc4044f117ddf71ae
Vardan Bansal <vardan.bansal@harness.io>
2020-06-23 17:07:40 +0530

    feat: [CDNG-258]: Removed redundant code

--- src/modules/common/components/YAMLBuilder/yaml.worker.ts
+++
@@ -1,9 +0,0 @@
-// @ts-nocheck
-import * as worker from 'monaco-editor/esm/vs/editor/editor.worker'
-import { YAMLWorker } from './yamlWorker'
-
-self.onmessage = () => {
-  worker.initialize((ctx, createData) => {
-    return new YAMLWorker(ctx, createData)
-  })
-}
