e46369c96458a875fd575980d7ba9920a49de00e
Vivek Kumar Bansal <5930351+vkbansal@users.noreply.github.com>
2020-12-10 15:45:37 +0530

    feat: Add accordion module (#821)

---
+++ src/modules/10-common/components/Accordion/Accordion.module.scss.d.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+// this is an auto-generated file
+declare const styles: {
+  readonly accordion: string
+  readonly summary: string
+  readonly label: string
+  readonly chevron: string
+  readonly panel: string
+  readonly details: string
+}
+export default styles
