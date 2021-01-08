cf2725a51eea2d2fcda54ab0ccc46e4990c93205
Kapil Kumawat <kapil.kumawat@harness.io>
2021-01-08 11:46:56 +0530

    fix: [CDNG-3432]: Fixed the pipeline studio error handling with cache (#1015)

--- src/modules/70-pipeline/components/PipelineStudio/PipelineStudio.tsx
+++ src/modules/70-pipeline/components/PipelineStudio/PipelineStudio.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import cx from 'classnames'
+import { Button, Layout, Text } from '@wings-software/uicore'
 import type { PipelinePathProps, ProjectPathProps, PathFn, PipelineType } from '@common/interfaces/RouteInterfaces'
 
+import { String } from 'framework/exports'
 import { PipelineCanvas } from './PipelineCanvas/PipelineCanvas'
 import { RightBar } from './RightBar/RightBar'
+import { PipelineContext } from './PipelineContext/PipelineContext'
 import css from './PipelineStudio.module.scss'
 
 export interface PipelineStudioProps {
@@ -18,29 +21,76 @@ export interface PipelineStudioProps {
   routePipelineProject: PathFn<PipelineType<ProjectPathProps>>
 }
 
-export const PipelineStudio: React.FC<PipelineStudioProps> = ({
-  children,
-  className = '',
-  routePipelineStudio,
-  routePipelineStudioUI,
-  routePipelineStudioYaml,
-  routePipelineDetail,
-  routePipelineList,
-  routePipelineProject
-}): JSX.Element => {
-  return (
-    <div className={cx(css.container, className)}>
-      <PipelineCanvas
-        toPipelineStudio={routePipelineStudio}
-        toPipelineStudioUI={routePipelineStudioUI}
-        toPipelineStudioYaml={routePipelineStudioYaml}
-        toPipelineDetail={routePipelineDetail}
-        toPipelineList={routePipelineList}
-        toPipelineProject={routePipelineProject}
-      >
-        {children}
-      </PipelineCanvas>
-      <RightBar />
-    </div>
-  )
+interface PipelineStudioState {
+  error?: boolean
+}
+export class PipelineStudio extends React.Component<PipelineStudioProps, PipelineStudioState> {
+  state: PipelineStudioState = { error: false }
+  context!: React.ContextType<typeof PipelineContext>
+  static contextType = PipelineContext
+
+  componentDidCatch(): boolean {
+    this.setState({ error: true })
+    return false
+  }
+
+  render(): JSX.Element {
+    const { error } = this.state
+    const { deletePipelineCache } = this.context
+    if (error) {
+      return (
+        <Layout.Vertical spacing="medium" padding="large">
+          <Text>
+            <String stringID="errorTitle" />
+          </Text>
+          <Text>
+            <String stringID="errorSubtitle" />
+          </Text>
+          <Layout.Horizontal style={{ alignItems: 'baseline' }}>
+            <Text>
+              <String stringID="please" />
+            </Text>
+            <Button
+              onClick={() => {
+                return deletePipelineCache().then(() => {
+                  window.location.reload()
+                })
+              }}
+              minimal
+            >
+              <String stringID="clickHere" />
+            </Button>
+            <Text>
+              <String stringID="errorHelp" />
+            </Text>
+          </Layout.Horizontal>
+        </Layout.Vertical>
+      )
+    }
+    const {
+      children,
+      className = '',
+      routePipelineStudio,
+      routePipelineStudioUI,
+      routePipelineStudioYaml,
+      routePipelineDetail,
+      routePipelineList,
+      routePipelineProject
+    } = this.props
+    return (
+      <div className={cx(css.container, className)}>
+        <PipelineCanvas
+          toPipelineStudio={routePipelineStudio}
+          toPipelineStudioUI={routePipelineStudioUI}
+          toPipelineStudioYaml={routePipelineStudioYaml}
+          toPipelineDetail={routePipelineDetail}
+          toPipelineList={routePipelineList}
+          toPipelineProject={routePipelineProject}
+        >
+          {children}
+        </PipelineCanvas>
+        <RightBar />
+      </div>
+    )
+  }
 }
