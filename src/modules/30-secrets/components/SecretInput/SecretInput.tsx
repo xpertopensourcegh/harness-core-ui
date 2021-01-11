e88f2722b3090de0f31adf424e350e65510027b3
Reetika <mallavarapu.reetika@harness.io>
2021-01-11 13:12:23 +0530

    feat: [CDNG-3472]: SecretInput Error Style Changes  (#1062)
    * feat: [CDNG-3472]: SecretInput Error Style Changes
    
    * feat: [CDNG-3472]: Snapshots updation

--- src/modules/30-secrets/components/SecretInput/SecretInput.tsx
+++ src/modules/30-secrets/components/SecretInput/SecretInput.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { connect, FormikContext } from 'formik'
-import { Layout, Icon, Button, Text } from '@wings-software/uicore'
+import { Layout, Icon, Button } from '@wings-software/uicore'
 
-import { get } from 'lodash-es'
+import { get, isPlainObject } from 'lodash-es'
+import { FormGroup, Intent } from '@blueprintjs/core'
 import useCreateOrSelectSecretModal from '@secrets/modals/CreateOrSelectSecretModal/useCreateOrSelectSecretModal'
 import type { SecretReference } from '@secrets/components/CreateOrSelectSecret/CreateOrSelectSecret'
 import type {
@@ -43,23 +44,30 @@ const SecretInput: React.FC<FormikSecretInput> = props => {
     },
     [name, onSuccess]
   )
+  const errorCheck = (): boolean =>
+    ((get(formik?.touched, name) || (formik?.submitCount && formik?.submitCount > 0)) &&
+      get(formik?.errors, name) &&
+      !isPlainObject(get(formik?.errors, name))) as boolean
+
   return (
-    <div className={'bp3-form-group'}>
-      {label ? <label className={'bp3-label'}>{label}</label> : null}
-      <Button
-        className={css.inputButton}
-        onClick={openCreateOrSelectSecretModal}
-        text={
-          <Layout.Horizontal width={'100%'} spacing={'xsmall'} style={{ alignItems: 'center' }}>
-            <Icon size={24} height={12} name={'key-main'} />
-            <span>{formik.values[name] ? formik.values[name]['name'] : i18n.btnLabel}</span>
-          </Layout.Horizontal>
-        }
-      />
-      {(get(formik?.touched, name) || (formik?.submitCount && formik?.submitCount > 0)) && formik.errors[name] ? (
-        <Text intent={'danger'}>{formik.errors[name]}</Text>
-      ) : null}
-    </div>
+    <FormGroup
+      helperText={errorCheck() ? get(formik?.errors, name) : null}
+      intent={errorCheck() ? Intent.DANGER : Intent.NONE}
+    >
+      <Layout.Vertical>
+        {label ? <label className={'bp3-label'}>{label}</label> : null}
+        <Button
+          className={css.inputButton}
+          onClick={openCreateOrSelectSecretModal}
+          text={
+            <Layout.Horizontal width={'100%'} spacing={'xsmall'} style={{ alignItems: 'center' }}>
+              <Icon size={24} height={12} name={'key-main'} />
+              <span>{formik.values[name] ? formik.values[name]['name'] : i18n.btnLabel}</span>
+            </Layout.Horizontal>
+          }
+        />
+      </Layout.Vertical>
+    </FormGroup>
   )
 }
 
