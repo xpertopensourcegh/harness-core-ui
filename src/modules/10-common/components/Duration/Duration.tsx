392cce22805505d70d5bd2add4f1a68792207ea2
Vivek Kumar Bansal <5930351+vkbansal@users.noreply.github.com>
2021-03-16 21:43:35 +0530

    chore: Duration uses utils from uicore (#2058)
    * chore: Duration uses utils from uicore
    
    * chore: Duration uses utils from uicore

--- src/modules/10-common/components/Duration/Duration.tsx
+++ src/modules/10-common/components/Duration/Duration.tsx
@@ -1,72 +1,26 @@
 import React, { useEffect, useState } from 'react'
-import { Text, TextProps } from '@wings-software/uicore'
+import { Text, TextProps, timeToDisplayText } from '@wings-software/uicore'
 import { isNil } from 'lodash-es'
-import i18n from './Duration.i18n'
+import { useStrings } from 'framework/exports'
 
 export interface DurationProps extends Omit<TextProps, 'icon'> {
   startTime?: number
   endTime?: number // if endTime is nullable, endTime is Date.now() and the duration is re-calculated by an interval
-  formatter?: string // subset of 'wdhms'
   durationText?: string // optional text to override the default `Duration: ` prefix
+  showMilliSeconds?: boolean
   icon?: TextProps['icon'] | null
 }
 
-/**
- * Idea from https://gist.github.com/THEtheChad/1297617. Copyright 2011 THEtheChad Elliott (MIT license).
- */
-interface DurationDiff {
-  w: number
-  d: number
-  h: number
-  m: number
-  s: number
-  ms: number
-}
-
-const durationInMillis: Omit<DurationDiff, 'ms'> = {
-  w: 604800000, // week
-  d: 86400000, // day
-  h: 3600000, // hour
-  m: 60000, // minutes
-  s: 1000 // second
-}
-
-const keys = ['w', 'd', 'h', 'm', 's']
-
-export function timeDelta(start: number, end: number, formatter = 'wdhms'): DurationDiff {
-  const delta: DurationDiff = { w: 0, d: 0, h: 0, m: 0, s: 0, ms: 0 }
-
-  delta.ms = Math.abs(end - start)
-
-  keys
-    .filter(key => formatter.includes(key))
-    .forEach(_key => {
-      const key = _key as keyof Omit<DurationDiff, 'ms'>
-
-      delta[key] = Math.floor(delta.ms / durationInMillis[key])
-      delta.ms -= delta[key] * durationInMillis[key]
-    })
-
-  return delta
-}
-
-export const Duration: React.FC<DurationProps> = ({
-  startTime,
-  endTime,
-  formatter,
-  durationText,
-  icon,
-  ...textProps
-}) => {
+export function Duration(props: DurationProps): React.ReactElement {
+  const { startTime, endTime, durationText, icon, showMilliSeconds, ...textProps } = props
   const [_endTime, setEndTime] = useState(endTime || Date.now())
-  React.useEffect(() => {
+  const { getString } = useStrings()
+
+  useEffect(() => {
     if (endTime) {
       setEndTime(endTime)
     }
-  }, [endTime])
-  const delta = startTime ? timeDelta(startTime, _endTime, formatter) : { w: 0, d: 0, h: 0, m: 0, s: 0 }
 
-  useEffect(() => {
     const timeoutId =
       (!endTime &&
         window.setInterval(() => {
@@ -79,19 +33,17 @@ export const Duration: React.FC<DurationProps> = ({
     }
   }, [endTime])
 
-  if (startTime && endTime && endTime - startTime < 1000) {
-    return (
-      <Text inline icon={isNil(icon) ? undefined : icon || 'hourglass'} {...textProps}>
-        {endTime - startTime}ms
-      </Text>
-    )
+  let delta = startTime ? Math.abs(startTime - _endTime) : 0
+
+  if (!showMilliSeconds) {
+    delta = Math.round(delta / 1000) * 1000
   }
 
-  const text = i18n.humanizeDuration(delta.w, delta.d, delta.h, delta.m, delta.s)
+  const text = timeToDisplayText(delta)
 
   return (
     <Text inline icon={isNil(icon) ? undefined : icon || 'hourglass'} {...textProps}>
-      {durationText || i18n.duration}
+      {durationText ?? getString('common.durationPrefix')}
       {text}
     </Text>
   )
--- src/modules/10-common/exports.ts
+++ src/modules/10-common/exports.ts
@@ -3,7 +3,6 @@
 //
 
 import * as StringUtils from './utils/StringUtils'
-import DurationI18n from './components/Duration/Duration.i18n'
 import type { DynamicPopoverHandlerBinding as _DynamicPopoverHandlerBinding } from './components/DynamicPopover/DynamicPopover'
 // YamlUtils can't be imported as it's bundled as ESM
 // in which Jest will fail
@@ -13,7 +12,7 @@ import * as rsql from './utils/rsql'
 export type DynamicPopoverHandlerBinding<T> = _DynamicPopoverHandlerBinding<T>
 export { DynamicPopover } from './components/DynamicPopover/DynamicPopover'
 export { Page } from './components/Page/Page'
-export { StringUtils, rsql /*, YamlUtils */, DurationI18n }
+export { StringUtils, rsql /*, YamlUtils */ }
 export { useConfirmationDialog } from './modals/ConfirmDialog/useConfirmationDialog'
 export { NavigationCheck } from './components/NavigationCheck/NavigationCheck'
 export { useToaster } from './components/Toaster/useToaster'
@@ -22,7 +21,7 @@ export { ReferenceSelect, MultiTypeReferenceInput } from './components/Reference
 export type { UserLabelProps } from './components/UserLabel/UserLabel'
 export { UserLabel } from './components/UserLabel/UserLabel'
 export type { DurationProps } from './components/Duration/Duration'
-export { Duration, timeDelta } from './components/Duration/Duration'
+export { Duration } from './components/Duration/Duration'
 export type { TimeAgoProps } from './components/TimeAgo/TimeAgo'
 export { TimeAgo } from './components/TimeAgo/TimeAgo'
 export { ExecutionStatus } from './constants/ExecutionStatus'
