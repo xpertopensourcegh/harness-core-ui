3994303f0995c6a7f39d92d4a8832678de287a32
Kapil Kumawat <kapil.kumawat@harness.io>
2021-03-22 13:11:33 +0530

    feat: Removed 70 module git connector, since with new design its not required (#2120)

--- src/modules/70-pipeline/components/connectors/GitConnector/CreateGitConnector.module.scss.d.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-// this is an auto-generated file
-declare const styles: {
-  readonly wrapper: string
-  readonly formContainer: string
-  readonly bottomButton: string
-  readonly heading: string
-}
-export default styles
