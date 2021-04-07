1c97932d836128d720a45d4f235892ec3054d747
Rishabh Kumar <rishabh.kumar@harness.io>
2021-04-07 15:57:57 +0530

    fix: [LWG-342]: simple log viewer not rendering log lines (#2377)

--- src/modules/10-common/components/MultiLogsViewer/SimpleLogViewer.tsx
+++ src/modules/10-common/components/MultiLogsViewer/SimpleLogViewer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import cx from 'classnames'
 import { Spinner } from '@blueprintjs/core'
 
-import { LogLineChunk, memoizedAnsiToJson } from './LogLine'
+import { LogLineProps, memoizedAnsiToJson } from './LogLine'
 import type { LineData } from './types'
 import css from './MultiLogsViewer.module.scss'
 
@@ -10,6 +10,44 @@ export interface SimpleLogViewerProps {
   data: string
   className?: string
   loading?: boolean
+}
+
+interface SimpleLogLineProps extends LogLineProps {
+  key?: string
+}
+
+const SimpleLogLine = (props: SimpleLogLineProps) => {
+  const {
+    data: { anserJson },
+    lineNumber,
+    key
+  } = props
+
+  return (
+    <div className={css.logLine} key={key}>
+      <div className={css.lineNumber}>{lineNumber}</div>
+      <div className={css.line}>
+        {anserJson?.map((row, i) => {
+          return (
+            row.content && (
+              <span
+                id="log-level"
+                className={`${css.logLineLevel} ${cx(
+                  {
+                    [`${row.bg}-bg`]: row.bg,
+                    [`${row.fg}-fg`]: row.fg
+                  },
+                  ...(row.decorations || []).map(p => `ansi-decoration-${p}`)
+                )}`}
+                key={`${row.content}_${i}`}
+                dangerouslySetInnerHTML={{ __html: row.content }}
+              />
+            )
+          )
+        })}
+      </div>
+    </div>
+  )
 }
 
 export function SimpleLogViewer(props: SimpleLogViewerProps): React.ReactElement {
@@ -27,7 +65,9 @@ export function SimpleLogViewer(props: SimpleLogViewerProps): React.ReactElement
         <Spinner />
       ) : (
         <pre>
-          <LogLineChunk data={linesData} chunkNumber={0} linesChunkSize={0} />
+          {linesData.map((row, index) => {
+            return <SimpleLogLine data={row} lineNumber={index + 1} key={`${row.raw}_${index}`} />
+          })}
         </pre>
       )}
     </div>
