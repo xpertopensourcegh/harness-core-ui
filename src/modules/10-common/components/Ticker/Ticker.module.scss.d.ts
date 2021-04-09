1bdbe68ac5b91a30762d9cc5d9293c7d95babb08
jasmeet-harness <78347581+jasmeet-harness@users.noreply.github.com>
2021-04-09 08:08:27 +0530

    feat: [CDNG-5557]: create Ticker component (#2222)
    * feat: [CDNG-5557]: create Ticker component
    
    * feat: [CDNG-5557]: get size from prop
    
    * feat: [CDNG-5557]: move to commons

---
+++ src/modules/10-common/components/Ticker/Ticker.module.scss.d.ts
@@ -0,0 +1,13 @@
+/* eslint-disable */
+// this is an auto-generated file
+declare const styles: {
+  readonly tickerContainer: string
+  readonly ticker: string
+  readonly iconContainer: string
+  readonly tickerValue: string
+  readonly tickerTopAlign: string
+  readonly tickerCenterAlign: string
+  readonly tickerReverseAlignment: string
+  readonly reverseAlignment: string
+}
+export default styles
---
+++ src/modules/10-common/components/Ticker/Ticker.tsx
@@ -0,0 +1,53 @@
+import * as React from 'react'
+import { Color, Icon } from '@wings-software/uicore'
+import cx from 'classnames'
+
+import css from './Ticker.module.scss'
+
+export enum TickerVerticalAlignment {
+  TOP = 'TickerVerticalAlignment.TOP',
+  CENTER = 'TickerVerticalAlignment.CENTER',
+  BOTTOM = 'TickerVerticalAlignment.BOTTOM'
+}
+
+export interface TickerProps {
+  decreaseMode?: boolean
+  color?: string
+  leftAlign?: boolean
+  tickerRightAligned?: boolean
+  value: string | React.ReactElement
+  verticalAlign?: TickerVerticalAlignment
+  size?: number
+}
+
+export const Ticker: React.FC<TickerProps> = props => {
+  const {
+    decreaseMode = false,
+    leftAlign = false,
+    tickerRightAligned = false,
+    verticalAlign = TickerVerticalAlignment.BOTTOM,
+    color = Color.GREEN_500,
+    size = 6,
+    value,
+    children
+  } = props
+  const iconName = decreaseMode ? 'main-caret-down' : 'main-caret-up'
+  return (
+    <div className={cx(css.tickerContainer, { [css.reverseAlignment]: leftAlign })}>
+      {children ? <>{children}</> : <></>}
+      <div
+        className={cx(
+          css.ticker,
+          { [css.tickerTopAlign]: verticalAlign === TickerVerticalAlignment.TOP },
+          { [css.tickerCenterAlign]: verticalAlign === TickerVerticalAlignment.CENTER },
+          { [css.tickerReverseAlignment]: tickerRightAligned }
+        )}
+      >
+        <div className={css.iconContainer}>
+          <Icon name={iconName} color={color} size={size} />
+        </div>
+        <div className={css.tickerValue}>{value}</div>
+      </div>
+    </div>
+  )
+}
