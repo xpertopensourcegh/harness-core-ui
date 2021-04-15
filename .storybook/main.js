9fc3ff8bd67b640c3ee23405f2080b0eef5d772d
Vivek Kumar Bansal <5930351+vkbansal@users.noreply.github.com>
2021-04-15 17:48:21 +0530

    chore: make storybook work with webpack 5 (#2521)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,31 +1,138 @@
 const webpackConfig = require('../webpack.config')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const MonacoWebpackPlugin = require(require.resolve('monaco-editor-webpack-plugin', { paths: [process.cwd()] }))
+const path = require('path')
+
+const CONTEXT = process.cwd()
 
 module.exports = {
+  core: {
+    builder: 'webpack5'
+  },
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   webpackFinal: config => {
     return {
       ...config,
+      cache: { type: 'filesystem', name: 'storybook' },
       module: {
         ...config.module,
         rules: [
           {
             test: /.*\/node_modules\/xterm\/.*\.css/,
-            use: 'ignore-loader'
+            use: 'null-loader'
           },
           {
-            test: /.*\/node_modules\/monaco-editor\/.*\.css/,
-            use: 'ignore-loader'
+            test: /.*\/node_modules\/monaco-editor\/.*/,
+            use: 'null-loader'
+          },
+          {
+            test: /.*\/node_modules\/@wings-software\/monaco-yaml\/.*/,
+            use: 'null-loader'
           },
           ...config.module.rules,
-          ...webpackConfig.module.rules
+          {
+            test: /\.m?js$/,
+            include: /node_modules/,
+            type: 'javascript/auto'
+          },
+          {
+            test: /\.(j|t)sx?$/,
+            exclude: /node_modules/,
+            use: [
+              {
+                loader: 'ts-loader',
+                options: {
+                  transpileOnly: true
+                }
+              }
+            ]
+          },
+          {
+            test: /\.module\.scss$/,
+            exclude: /node_modules/,
+            use: [
+              MiniCssExtractPlugin.loader,
+              {
+                loader: '@wings-software/css-types-loader',
+                options: {
+                  prettierConfig: CONTEXT
+                }
+              },
+              {
+                loader: 'css-loader',
+                options: {
+                  importLoaders: 1,
+                  modules: {
+                    mode: 'local',
+                    localIdentName: '[name]_[local]_[hash:base64:6]',
+                    exportLocalsConvention: 'camelCaseOnly'
+                  }
+                }
+              },
+              {
+                loader: 'sass-loader',
+                options: {
+                  sassOptions: {
+                    includePaths: [path.join(CONTEXT, 'src')]
+                  },
+                  sourceMap: false,
+                  implementation: require('sass')
+                }
+              }
+            ]
+          },
+          {
+            test: /(?<!\.module)\.scss$/,
+            exclude: /node_modules/,
+            use: [
+              MiniCssExtractPlugin.loader,
+              {
+                loader: 'css-loader',
+                options: {
+                  importLoaders: 1,
+                  modules: false
+                }
+              },
+              {
+                loader: 'sass-loader',
+                options: {
+                  sassOptions: {
+                    includePaths: [path.join(CONTEXT, 'src')]
+                  },
+                  implementation: require('sass')
+                }
+              }
+            ]
+          },
+          {
+            test: /\.ya?ml$/,
+            type: 'json',
+            use: [
+              {
+                loader: 'yaml-loader'
+              }
+            ]
+          }
+          // ...webpackConfig.module.rules
         ]
       },
       resolve: {
         ...config.resolve,
-        ...webpackConfig.resolve
+        ...webpackConfig.resolve,
+        fallback: { assert: require.resolve('assert/') }
       },
-      plugins: [...config.plugins, ...webpackConfig.plugins],
+      plugins: [
+        ...config.plugins,
+        new MiniCssExtractPlugin({
+          filename: 'static/[name].css',
+          chunkFilename: 'static/[name].[id].css'
+        }),
+        new MonacoWebpackPlugin({
+          // available options are documented at https://github.com/Microsoft/monaco-editor-webpack-plugin#options
+          languages: ['yaml', 'shell', 'powershell']
+        })
+      ],
       stats: 'minimal'
     }
   }
