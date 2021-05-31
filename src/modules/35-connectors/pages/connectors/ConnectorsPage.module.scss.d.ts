7bbb5a1f8f867048f93adb0f9598430ea97d650c
Meenakshi Raikwar <meenakshi.raikwar@harness.io>
2021-05-31 13:42:41 +0530

    feat:  [CDNG-8237]: Fixed background color in secret and connectors l… (#3387)
    * feat:  [CDNG-8237]: Fixed background color in secret and connectors listing page
    
    * feat:  [CDNG-8237]: Fixed background color in secret and connectors listing page

--- src/modules/35-connectors/pages/connectors/ConnectorsPage.module.scss.d.ts
+++ src/modules/35-connectors/pages/connectors/ConnectorsPage.module.scss.d.ts
@@ -1,7 +1,6 @@
 /* eslint-disable */
 // this is an auto-generated file
 declare const styles: {
-  readonly listWrapper: string
   readonly header: string
   readonly gitFilter: string
   readonly listBody: string
