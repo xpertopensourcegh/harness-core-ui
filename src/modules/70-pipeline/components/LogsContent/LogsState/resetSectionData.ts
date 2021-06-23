0b965a49db6108d891b1911a72daa0f025a26555
Vivek Kumar Bansal <5930351+vkbansal@users.noreply.github.com>
2021-06-23 20:30:33 +0530

    fix: logs crash when unit status is present (#4045)
    * fix: logs crash when unit status is present
    
    * fix: logs crash when unit status is present

--- src/modules/70-pipeline/components/LogsContent/LogsState/resetSectionData.ts
+++ src/modules/70-pipeline/components/LogsContent/LogsState/resetSectionData.ts
@@ -1,12 +1,15 @@
 import produce from 'immer'
-import { set } from 'lodash-es'
+import { get, set } from 'lodash-es'
 import type { Action, ActionType, State } from './types'
 
 export function resetSectionData(state: State, action: Action<ActionType.ResetSection>): State {
   const { payload } = action
+  const unit = get(state, ['dataMap', payload])
+
+  if (!unit) return state
 
   return produce(state, draft => {
-    set(draft.dataMap[payload], 'status', state.dataMap[payload].unitStatus)
+    set(draft.dataMap[payload], 'status', unit.unitStatus)
     set(draft.dataMap[payload], 'isOpen', false)
   })
 }
