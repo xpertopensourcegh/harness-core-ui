a246b960de938251ccd512c06097122ee8488373
conormurray95 <conor.murray@harness.io>
2021-06-28 17:21:00 +0100

    fix: [FFM-1120] Fix target csv upload delete handler (#4170)

--- src/modules/75-cf/pages/target-management/targets/CreateTargetModal.tsx
+++ src/modules/75-cf/pages/target-management/targets/CreateTargetModal.tsx
@@ -143,12 +143,12 @@ const FileUpload: React.FC<{ onChange: (targets: TargetData[]) => void }> = ({ o
     _createdItems,
     _items
   ) => {
-    setTargets(
-      selectedItems.map(arg => {
-        const { label, value } = arg as { label: string; value: string }
-        return { name: value, identifier: label }
-      })
-    )
+    const updatedTargets = selectedItems.map(arg => {
+      const { label, value } = arg as { label: string; value: string }
+      return { name: value, identifier: label }
+    })
+    setTargets(updatedTargets)
+    onChange(updatedTargets)
   }
 
   return (
