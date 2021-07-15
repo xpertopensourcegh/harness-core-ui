21c511cc0fbc3fac841f029e85f944e4eb67d2ab
Swaraj Cheguri <73115842+swarajcheguri-harness@users.noreply.github.com>
2021-07-15 11:49:50 +0530

    feat: [PL-14964]: commit message jira ticket check (#4391)
    * feat: [PL-14964]: jira update script
    
    * feat: [PL-14964]: update code
    
    * feat: [PL-14964]: update code
    
    * feat: [PL-14964]: update code
    
    * feat: [PL-14964]: update code
    
    * feat: [PL-14964]: update code
    
    * feat: [PL-14964]: update code
    
    * feat: [PL-14964]: update code
    
    * with working script
    
    * feat: [PL-14964]: with space correction
    
    * feat: [PL-14964]: with updated script
    
    * feat: [PL-14964]: precommit jira ticket check
    
    * feat: [PL-14964]:with precommit jira ticket check
    
    * feat: [PL-14964]:with env variable
    
    * feat: [PL-14964]:with env variable
    
    * feat: [PL-14964]:remove env variable

--- scripts/lint-commit-message.js
+++ scripts/lint-commit-message.js
@@ -1,8 +1,9 @@
 const COMMIT_MSG = process.argv[2]
-const COMMIT_REGEX = /^(revert: )?(feat|fix|docs|style|refactor|perf|test|chore)(\(.+\))?: .{1,50}/
-
+const JIRA_TKT_CHECK= process.env.JIRA_TKT_CHECK==='true'
+const COMMIT_REGEX = JIRA_TKT_CHECK ? /^(revert: )?(feat|fix|docs|style|refactor|perf|test|chore)(\(.+\))?:\s\[.*-\d*\]: .{1,50}/ :/^(revert: )?(feat|fix|docs|style|refactor|perf|test|chore)(\(.+\))?: .{1,50}/
+const ERROR_MSG= JIRA_TKT_CHECK ? 'Commit messages must be "fix/feat/docs/style/refactor/perf/test/chore: [HAR-<ticket number>]: <changes>"':'Commit messages must be "fix/feat/docs/style/refactor/perf/test/chore: <changes>"'
 if (!COMMIT_REGEX.test(COMMIT_MSG)) {
-  console.log('Commit messages must be "fix/feat/docs/style/refactor/perf/test/chore: <changes>"')
+  console.log(ERROR_MSG)
   console.log(`But got: "${COMMIT_MSG}"`)
   process.exit(1)
 }
