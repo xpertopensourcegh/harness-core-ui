1ac9c49a2a3b4daa4fd5c4cc3d11099e339c5e5e
Harshil Garg <harshil.garg@harness.io>
2021-07-18 18:16:17 +0530

    feat: [CVNG=2964]: fixing the runtime screen (#4496)

--- src/modules/85-cv/components/PipelineSteps/ContinousVerification/components/ContinousVerificationInputSetStep/components/ContinousVerificationInputSetStep.utils.ts
+++ src/modules/85-cv/components/PipelineSteps/ContinousVerification/components/ContinousVerificationInputSetStep/components/ContinousVerificationInputSetStep.utils.ts
@@ -1,4 +1,5 @@
 import { RUNTIME_INPUT_VALUE } from '@wings-software/uicore'
+import { StepType } from '@pipeline/components/PipelineSteps/PipelineStepInterface'
 import type { NgPipeline } from 'services/cd-ng'
 import type { serviceAndEnvData } from '../types'
 
@@ -6,14 +7,14 @@ export const getInfraAndServiceData = (
   pipeline: { pipeline: NgPipeline } | undefined,
   formik: any
 ): serviceAndEnvData => {
-  const currentStageFromPipeline = pipeline?.pipeline?.stages?.find(
-    (el: any) => el?.stage?.identifier === formik?.values?.identifier
+  const stageFromPipelineHavingVerify = pipeline?.pipeline?.stages?.find(
+    (el: any) => !!el?.stage?.spec?.execution?.steps?.find((step: any) => step?.step?.type === StepType.Verify)
   )
   const currentStageFromForm = formik?.values?.stages?.find(
-    (el: any) => el?.stage?.identifier === formik?.values?.identifier
+    (el: any) => el?.stage?.identifier === stageFromPipelineHavingVerify?.stage?.identifier
   )
-  let serviceIdentifierData = currentStageFromPipeline?.stage?.spec?.serviceConfig?.serviceRef
-  let envIdentifierData = currentStageFromPipeline?.stage?.spec?.infrastructure?.environmentRef
+  let serviceIdentifierData = stageFromPipelineHavingVerify?.stage?.spec?.serviceConfig?.serviceRef
+  let envIdentifierData = stageFromPipelineHavingVerify?.stage?.spec?.infrastructure?.environmentRef
   if (serviceIdentifierData === RUNTIME_INPUT_VALUE) {
     serviceIdentifierData = currentStageFromForm?.stage?.spec?.serviceConfig?.serviceRef
   }
