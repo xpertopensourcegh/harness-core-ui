25776477a0c442b638e7cd43dc86dec46b953195
Nana Xu <nana.xu@harness.io>
2021-08-10 17:51:58 -0500

    feat: [GTM-360]: stub telemetry for prem (#4884)

--- src/framework/utils/Telemetry.ts
+++ src/framework/utils/Telemetry.ts
@@ -1,5 +1,13 @@
 import Telemetry from '@wings-software/telemetry'
 
-const TelemetryInstance = new Telemetry(window.segmentToken || 'exa6lo7CnJXqKnR83itMpHYLY5fiajft')
+const stubTelemetry = {
+  identify: () => void 0,
+  track: () => void 0,
+  page: () => void 0
+}
+
+const TelemetryInstance = __ON_PREM__
+  ? stubTelemetry
+  : new Telemetry(window.segmentToken || 'exa6lo7CnJXqKnR83itMpHYLY5fiajft')
 
 export default TelemetryInstance
