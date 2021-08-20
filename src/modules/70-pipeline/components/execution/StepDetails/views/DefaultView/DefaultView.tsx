a86e860aec1d1c4548a130ec53f943f3f97d4815
Ishant Mehta <43949150+ishantmehtaharness@users.noreply.github.com>
2021-08-20 12:58:02 +0530

    feat: [PIE-979]: Use Tabs from UICore rather than blueprint directly (#5009)

--- src/modules/70-pipeline/components/execution/StepDetails/views/DefaultView/DefaultView.tsx
+++ src/modules/70-pipeline/components/execution/StepDetails/views/DefaultView/DefaultView.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { merge } from 'lodash-es'
-import { Tabs } from '@blueprintjs/core'
+import { Tabs, Tab } from '@wings-software/uicore'
 
 import { useStrings } from 'framework/strings'
 import { isExecutionWaitingForIntervention } from '@pipeline/utils/statusHelpers'
@@ -40,44 +40,45 @@ export function DefaultView(props: StepDetailProps): React.ReactElement {
   }, [step.identifier, isManualInterruption])
 
   return (
-    <Tabs
-      id="step-details"
-      className={css.tabs}
-      selectedTabId={activeTab}
-      onChange={newTab => {
-        manuallySelected.current = true
-        setActiveTab(newTab as StepDetailTab)
-      }}
-      renderActiveTabPanelOnly
-    >
-      <Tabs.Tab id={StepDetailTab.STEP_DETAILS} title={getString('details')} panel={<StepDetailsTab step={step} />} />
-      {shouldShowInputOutput && (
-        <Tabs.Tab
-          id={StepDetailTab.INPUT}
-          title={getString('common.input')}
-          panel={<InputOutputTab baseFqn={step.baseFqn} mode="input" data={step.stepParameters} />}
-        />
-      )}
-      {shouldShowInputOutput && (
-        <Tabs.Tab
-          id={StepDetailTab.OUTPUT}
-          title={getString('outputLabel')}
-          panel={
-            <InputOutputTab
-              baseFqn={step.baseFqn}
-              mode="output"
-              data={Array.isArray(step.outcomes) ? { output: merge({}, ...step.outcomes) } : step.outcomes}
-            />
-          }
-        />
-      )}
-      {isManualInterruption ? (
-        <Tabs.Tab
-          id={StepDetailTab.MANUAL_INTERVENTION}
-          title={getString('pipeline.failureStrategies.strategiesLabel.ManualIntervention')}
-          panel={<ManualInterventionTab step={step} allowedStrategies={failureStrategies} />}
-        />
-      ) : null}
-    </Tabs>
+    <div className={css.tabs}>
+      <Tabs
+        id="step-details"
+        selectedTabId={activeTab}
+        onChange={newTab => {
+          manuallySelected.current = true
+          setActiveTab(newTab as StepDetailTab)
+        }}
+        renderAllTabPanels={false}
+      >
+        <Tab id={StepDetailTab.STEP_DETAILS} title={getString('details')} panel={<StepDetailsTab step={step} />} />
+        {shouldShowInputOutput && (
+          <Tab
+            id={StepDetailTab.INPUT}
+            title={getString('common.input')}
+            panel={<InputOutputTab baseFqn={step.baseFqn} mode="input" data={step.stepParameters} />}
+          />
+        )}
+        {shouldShowInputOutput && (
+          <Tab
+            id={StepDetailTab.OUTPUT}
+            title={getString('outputLabel')}
+            panel={
+              <InputOutputTab
+                baseFqn={step.baseFqn}
+                mode="output"
+                data={Array.isArray(step.outcomes) ? { output: merge({}, ...step.outcomes) } : step.outcomes}
+              />
+            }
+          />
+        )}
+        {isManualInterruption ? (
+          <Tab
+            id={StepDetailTab.MANUAL_INTERVENTION}
+            title={getString('pipeline.failureStrategies.strategiesLabel.ManualIntervention')}
+            panel={<ManualInterventionTab step={step} allowedStrategies={failureStrategies} />}
+          />
+        ) : null}
+      </Tabs>
+    </div>
   )
 }
