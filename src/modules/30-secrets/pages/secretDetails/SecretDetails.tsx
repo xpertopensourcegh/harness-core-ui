52dcdd9417dc4e1181cfd04f2907a87248e50df5
Shashidhar Gani <shashidhar.gani@harness.io>
2021-09-20 10:09:40 +0530

    fix: [PL-19963]: Fixed error handler for updateSecretYaml (#5359)
    * fix: [PL-19963]: Fixed error handler for updateSecretYaml
    
    * fix: [PL-19963]: SonarQube fixes
    
    * feat: [PL-19963]: SonarQube fixes
    
    * fix: [PL-19963]: Moved YAML SecretDetails code into a separate component
    
    * fix: [PL-19963]: Code Refactor

--- src/modules/30-secrets/pages/secretDetails/SecretDetails.tsx
+++ src/modules/30-secrets/pages/secretDetails/SecretDetails.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { parse } from 'yaml'
-import { omit, without } from 'lodash-es'
+import { omit, without, defaultTo } from 'lodash-es'
 import { Layout, Container, Button, ButtonVariation } from '@wings-software/uicore'
 
 import {
@@ -10,7 +10,8 @@ import {
   ResponseSecretResponseWrapper,
   useGetYamlSchema,
   useGetYamlSnippetMetadata,
-  useGetYamlSnippet
+  useGetYamlSnippet,
+  SecretResponseWrapper
 } from 'services/cd-ng'
 
 import { useStrings } from 'framework/strings'
@@ -27,8 +28,7 @@ import { useDocumentTitle } from '@common/hooks/useDocumentTitle'
 import { ResourceType } from '@rbac/interfaces/ResourceType'
 import RbacButton from '@rbac/components/Button/Button'
 import { PermissionIdentifier } from '@rbac/interfaces/PermissionIdentifier'
-import { SelectedView } from '@common/components/VisualYamlToggle/VisualYamlToggle'
-import VisualYamlToggle from '@common/components/VisualYamlToggle/VisualYamlToggle'
+import VisualYamlToggle, { SelectedView } from '@common/components/VisualYamlToggle/VisualYamlToggle'
 import { yamlStringify } from '@common/utils/YamlHelperMethods'
 import ViewSecretDetails from './views/ViewSecretDetails'
 import './SecretDetails.module.scss'
@@ -38,28 +38,49 @@ interface SecretDetailsProps {
   refetch?: () => void
 }
 
+interface YAMLSecretDetailsProps {
+  refetch?: () => void
+  secretData: SecretResponseWrapper
+  edit?: boolean
+  setEdit: React.Dispatch<React.SetStateAction<boolean | undefined>>
+}
+
 const yamlSanityConfig = {
   removeEmptyString: false
 }
 
-const SecretDetails: React.FC<SecretDetailsProps> = props => {
+const YAMLSecretDetails: React.FC<YAMLSecretDetailsProps> = ({ refetch, secretData, edit, setEdit }) => {
+  const { getString } = useStrings()
+  const { showSuccess, showError } = useToaster()
   const { accountId, projectIdentifier, orgIdentifier, secretId } = useParams<
     ProjectPathProps & SecretsPathProps & ModulePathParams
   >()
-  const { getString } = useStrings()
-  const { showSuccess, showError } = useToaster()
-  const [edit, setEdit] = useState<boolean>()
-  const [mode, setMode] = useState<SelectedView>(SelectedView.VISUAL)
-  const [fieldsRemovedFromYaml, setFieldsRemovedFromYaml] = useState(['draft', 'createdAt', 'updatedAt'])
   const [yamlHandler, setYamlHandler] = React.useState<YamlBuilderHandlerBinding | undefined>()
   const [snippetFetchResponse, setSnippetFetchResponse] = React.useState<SnippetFetchResponse>()
-  const data = props.secretData
-  const { mutate: updateSecretYaml } = usePutSecretViaYaml({
-    identifier: secretId,
-    queryParams: { accountIdentifier: accountId, orgIdentifier, projectIdentifier },
-    // this is required to make sure backend understands the content type correctly
-    requestOptions: { headers: { 'content-type': 'application/yaml' } }
+  const [fieldsRemovedFromYaml, setFieldsRemovedFromYaml] = useState(['draft', 'createdAt', 'updatedAt'])
+
+  useEffect(() => {
+    if (secretData.secret.type !== 'SecretText') {
+      return
+    }
+
+    switch ((secretData.secret.spec as SecretTextSpecDTO)?.valueType) {
+      case 'Inline':
+        setFieldsRemovedFromYaml([...fieldsRemovedFromYaml, 'secret.spec.value'])
+        break
+      case 'Reference':
+        // 'value' field should be persisted in visual->yaml transistion for reference type
+        setFieldsRemovedFromYaml(without(fieldsRemovedFromYaml, 'secret.spec.value'))
+        break
+    }
+  }, [secretData])
+
+  const { data: secretSchema } = useGetYamlSchema({
+    queryParams: {
+      entityType: 'Secrets'
+    }
   })
+
   const { data: snippetData } = useGetYamlSnippetMetadata({
     queryParams: {
       tags: getSnippetTags('Secrets')
@@ -68,41 +89,7 @@ const SecretDetails: React.FC<SecretDetailsProps> = props => {
       arrayFormat: 'repeat'
     }
   })
-  const { data: secretSchema } = useGetYamlSchema({
-    queryParams: {
-      entityType: 'Secrets'
-    }
-  })
 
-  const [secretData, setSecretData] = useState(data?.data)
-
-  const { openCreateSSHCredModal } = useCreateSSHCredModal({ onSuccess: props.refetch })
-  const { openCreateSecretModal } = useCreateUpdateSecretModal({ onSuccess: props.refetch })
-  const handleSaveYaml = async (): Promise<void> => {
-    const yamlData = yamlHandler?.getLatestYaml()
-    let jsonData
-    try {
-      jsonData = parse(yamlData || '')
-    } catch (err) {
-      showError(err.message)
-    }
-
-    if (yamlData && jsonData) {
-      try {
-        await updateSecretYaml(yamlData as any)
-        showSuccess(getString('secrets.secret.updateSuccess'))
-        setEdit(false)
-        props.refetch?.()
-      } catch (err) {
-        showError(err.data?.errors[0]?.error || err.data.message)
-      }
-    }
-  }
-  useDocumentTitle([getString('overview'), secretData?.secret.name || '', getString('common.secrets')])
-
-  useEffect(() => {
-    setSecretData(data?.data)
-  }, [data?.data])
   const {
     data: snippet,
     refetch: refetchSnippet,
@@ -113,6 +100,15 @@ const SecretDetails: React.FC<SecretDetailsProps> = props => {
     identifier: '',
     lazy: true
   })
+
+  const onSnippetCopy = async (identifier: string): Promise<void> => {
+    cancel()
+    await refetchSnippet({
+      pathParams: {
+        identifier
+      }
+    })
+  }
 
   useEffect(() => {
     let snippetStr = ''
@@ -128,27 +124,33 @@ const SecretDetails: React.FC<SecretDetailsProps> = props => {
     })
   }, [isFetchingSnippet])
 
-  const onSnippetCopy = async (identifier: string): Promise<void> => {
-    cancel()
-    await refetchSnippet({
-      pathParams: {
-        identifier
-      }
-    })
-  }
-  useEffect(() => {
-    if (secretData?.secret.type === 'SecretText') {
-      switch ((secretData?.secret.spec as SecretTextSpecDTO)?.valueType) {
-        case 'Inline':
-          setFieldsRemovedFromYaml([...fieldsRemovedFromYaml, 'secret.spec.value'])
-          break
-        case 'Reference':
-          // 'value' field should be persisted in visual->yaml transistion for reference type
-          setFieldsRemovedFromYaml(without(fieldsRemovedFromYaml, 'secret.spec.value'))
-          break
+  const { mutate: updateSecretYaml } = usePutSecretViaYaml({
+    identifier: secretId,
+    queryParams: { accountIdentifier: accountId, orgIdentifier, projectIdentifier },
+    // this is required to make sure backend understands the content type correctly
+    requestOptions: { headers: { 'content-type': 'application/yaml' } }
+  })
+
+  const handleSaveYaml = async (): Promise<void> => {
+    const yamlData = yamlHandler?.getLatestYaml()
+    let jsonData
+    try {
+      jsonData = parse(defaultTo(yamlData, ''))
+    } catch (err) {
+      showError(err.message)
+    }
+
+    if (yamlData && jsonData) {
+      try {
+        await updateSecretYaml(yamlData as any)
+        showSuccess(getString('secrets.secret.updateSuccess'))
+        setEdit(false)
+        refetch?.()
+      } catch (err) {
+        showError(defaultTo(err.data?.message, err.message))
       }
     }
-  }, [secretData])
+  }
 
   const { openDialog } = useConfirmationDialog({
     cancelButtonText: getString('cancel'),
@@ -158,7 +160,7 @@ const SecretDetails: React.FC<SecretDetailsProps> = props => {
     onCloseDialog: isConfirmed => {
       if (isConfirmed) {
         setEdit(false)
-        props.refetch?.()
+        refetch?.()
       }
     }
   })
@@ -168,12 +170,101 @@ const SecretDetails: React.FC<SecretDetailsProps> = props => {
     event.stopPropagation()
     openDialog()
   }
-  if (!secretData)
+
+  return (
+    <Container>
+      {edit && (
+        <>
+          <YamlBuilder
+            entityType={'Secrets'}
+            fileName={`${secretData.secret.name}.yaml`}
+            existingJSON={omit(secretData, fieldsRemovedFromYaml)}
+            bind={setYamlHandler}
+            onSnippetCopy={onSnippetCopy}
+            snippetFetchResponse={snippetFetchResponse}
+            schema={secretSchema?.data}
+            isReadOnlyMode={false}
+            snippets={snippetData?.data?.yamlSnippets}
+            yamlSanityConfig={yamlSanityConfig}
+            showSnippetSection={false}
+          />
+          <Layout.Horizontal spacing="medium">
+            <Button
+              intent="primary"
+              text={getString('save')}
+              onClick={handleSaveYaml}
+              margin={{ top: 'large' }}
+              variation={ButtonVariation.PRIMARY}
+            />
+            <Button
+              text={getString('cancel')}
+              margin={{ top: 'large' }}
+              onClick={resetEditor}
+              variation={ButtonVariation.TERTIARY}
+            />
+          </Layout.Horizontal>
+        </>
+      )}
+      {!edit && (
+        <YamlBuilder
+          entityType={'Secrets'}
+          existingJSON={omit(secretData, fieldsRemovedFromYaml)}
+          fileName={`${secretData.secret.name}.yaml`}
+          isReadOnlyMode={true}
+          showSnippetSection={false}
+          onEnableEditMode={() => setEdit(true)}
+          yamlSanityConfig={yamlSanityConfig}
+        />
+      )}
+    </Container>
+  )
+}
+
+const SecretDetails: React.FC<SecretDetailsProps> = props => {
+  const { getString } = useStrings()
+  const [edit, setEdit] = useState<boolean>()
+  const [mode, setMode] = useState<SelectedView>(SelectedView.VISUAL)
+  const data = props.secretData
+
+  const [secretData, setSecretData] = useState(data?.data)
+
+  const { openCreateSSHCredModal } = useCreateSSHCredModal({ onSuccess: props.refetch })
+  const { openCreateSecretModal } = useCreateUpdateSecretModal({ onSuccess: props.refetch })
+
+  useDocumentTitle([getString('overview'), defaultTo(secretData?.secret.name, ''), getString('common.secrets')])
+
+  useEffect(() => {
+    setSecretData(data?.data)
+  }, [data?.data])
+
+  if (!secretData) {
     return (
       <Container flex={{ align: 'center-center' }} padding="xxlarge">
         {getString('noData')}
       </Container>
     )
+  }
+
+  const handleVisualMode = (): void => {
+    if (secretData.secret.type === 'SSHKey') {
+      openCreateSSHCredModal(data?.data?.secret)
+      return
+    }
+    openCreateSecretModal(secretData.secret.type, {
+      identifier: secretData.secret?.identifier,
+      orgIdentifier: secretData.secret?.orgIdentifier,
+      projectIdentifier: secretData.secret?.projectIdentifier
+    } as SecretIdentifiers)
+  }
+
+  const handleEdit = (): void => {
+    if (mode === SelectedView.VISUAL) {
+      handleVisualMode()
+      return
+    }
+    setEdit(true)
+  }
+
   return (
     <>
       <PageHeader size="standard" title={getString('overview')} />
@@ -191,17 +282,7 @@ const SecretDetails: React.FC<SecretDetailsProps> = props => {
               <RbacButton
                 text={getString('editDetails')}
                 icon="edit"
-                onClick={() => {
-                  mode === SelectedView.VISUAL
-                    ? secretData.secret.type === 'SSHKey'
-                      ? openCreateSSHCredModal(data?.data?.secret)
-                      : openCreateSecretModal(secretData.secret.type, {
-                          identifier: secretData.secret?.identifier,
-                          orgIdentifier: secretData.secret?.orgIdentifier,
-                          projectIdentifier: secretData.secret?.projectIdentifier
-                        } as SecretIdentifiers)
-                    : setEdit(true)
-                }}
+                onClick={handleEdit}
                 permission={{
                   permission: PermissionIdentifier.UPDATE_SECRET,
                   resource: {
@@ -215,51 +296,7 @@ const SecretDetails: React.FC<SecretDetailsProps> = props => {
           )}
         </Container>
         {mode === SelectedView.YAML ? (
-          <Container>
-            {edit && (
-              <YamlBuilder
-                entityType={'Secrets'}
-                fileName={`${secretData.secret.name}.yaml`}
-                existingJSON={omit(secretData, fieldsRemovedFromYaml)}
-                bind={setYamlHandler}
-                onSnippetCopy={onSnippetCopy}
-                snippetFetchResponse={snippetFetchResponse}
-                schema={secretSchema?.data}
-                isReadOnlyMode={false}
-                snippets={snippetData?.data?.yamlSnippets}
-                yamlSanityConfig={yamlSanityConfig}
-                showSnippetSection={false}
-              />
-            )}
-            {!edit && (
-              <YamlBuilder
-                entityType={'Secrets'}
-                existingJSON={omit(secretData, fieldsRemovedFromYaml)}
-                fileName={`${secretData.secret.name}.yaml`}
-                isReadOnlyMode={true}
-                showSnippetSection={false}
-                onEnableEditMode={() => setEdit(true)}
-                yamlSanityConfig={yamlSanityConfig}
-              />
-            )}
-            {edit && (
-              <Layout.Horizontal spacing="medium">
-                <Button
-                  intent="primary"
-                  text={getString('save')}
-                  onClick={handleSaveYaml}
-                  margin={{ top: 'large' }}
-                  variation={ButtonVariation.PRIMARY}
-                />
-                <Button
-                  text={getString('cancel')}
-                  margin={{ top: 'large' }}
-                  onClick={resetEditor}
-                  variation={ButtonVariation.TERTIARY}
-                />
-              </Layout.Horizontal>
-            )}
-          </Container>
+          <YAMLSecretDetails refetch={props.refetch} secretData={secretData} edit={edit} setEdit={setEdit} />
         ) : (
           //View in Visual Mode
           <ViewSecretDetails secret={secretData} />
