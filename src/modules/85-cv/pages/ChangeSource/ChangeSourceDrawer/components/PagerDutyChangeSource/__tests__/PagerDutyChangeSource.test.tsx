69d534e64d5fe28288422165573aa52d5c36226a
deepesh-ui <81554936+deepesh-ui@users.noreply.github.com>
2021-10-08 00:30:32 +0530

    feat: [CVNG-3565]: Updated tests for PagerDuty ChangeSource (#5673)

--- src/modules/85-cv/pages/ChangeSource/ChangeSourceDrawer/components/PagerDutyChangeSource/__tests__/PagerDutyChangeSource.test.tsx
+++ src/modules/85-cv/pages/ChangeSource/ChangeSourceDrawer/components/PagerDutyChangeSource/__tests__/PagerDutyChangeSource.test.tsx
@@ -1,11 +1,46 @@
 import React from 'react'
-import { render, waitFor } from '@testing-library/react'
+import { Formik } from 'formik'
+import { noop } from 'lodash-es'
+import { act, fireEvent, render, waitFor } from '@testing-library/react'
 import { TestWrapper } from '@common/utils/testUtils'
 import * as cvServices from 'services/cv'
 import PagerDutyChangeSource from '../PagerDutyChangeSource'
 
+jest.mock('services/cd-ng', () => ({
+  ...jest.requireActual('services/cd-ng'),
+  useGetConnectorList: () => {
+    return {
+      data: {},
+      refetch: jest.fn()
+    }
+  },
+  useGetConnector: () => {
+    return {
+      data: {},
+      refetch: jest.fn()
+    }
+  }
+}))
+
+interface InitValue {
+  spec: {
+    connectorRef?: string
+    pagerDutyServiceId?: string
+  }
+}
+
+const TestComponent = ({ initialValues }: { initialValues: InitValue }): React.ReactElement => (
+  <TestWrapper>
+    <Formik initialValues={initialValues} onSubmit={noop}>
+      {formik => {
+        return <PagerDutyChangeSource formik={formik} isEdit={true} />
+      }}
+    </Formik>
+  </TestWrapper>
+)
+
 describe('Test PagerDuty Change Source', () => {
-  test('PagerDuty ChangeSource renders in create mode', async () => {
+  test('PagerDuty ChangeSource shows empty service message', async () => {
     jest.spyOn(cvServices, 'useGetServicesFromPagerDuty').mockImplementation(
       () =>
         ({
@@ -16,21 +51,19 @@ describe('Test PagerDuty Change Source', () => {
         } as any)
     )
     const { container, getByText } = render(
-      <TestWrapper>
-        <PagerDutyChangeSource
-          formik={
-            {
-              values: {
-                spec: {
-                  pagerDutyServiceId: 'cvng',
-                  connectorRef: 'PagerDutyConnector'
-                }
-              }
-            } as any
+      <TestComponent
+        initialValues={{
+          spec: {
+            pagerDutyServiceId: '',
+            connectorRef: 'PagerDutyConnector'
           }
-        />
-      </TestWrapper>
+        }}
+      />
     )
+
+    fireEvent.click(container.querySelector(`.bp3-input-action [data-icon="chevron-down"]`)!)
+    const menuItemLabels = container.querySelectorAll('[class*="menuItemLabel"]')
+    expect(menuItemLabels.length).toEqual(0)
 
     await waitFor(() => expect(getByText('cv.changeSource.PageDuty.pagerDutyService')).toBeTruthy())
     await waitFor(() => expect(getByText('cv.changeSource.PageDuty.pagerDutyEmptyService')).toBeTruthy())
@@ -40,33 +73,46 @@ describe('Test PagerDuty Change Source', () => {
   })
 
   test('PagerDuty ChangeSource renders in edit mode', async () => {
+    const refetch = jest.fn()
+
     jest.spyOn(cvServices, 'useGetServicesFromPagerDuty').mockImplementation(
       () =>
         ({
           loading: false,
           error: null,
-          data: {},
-          refetch: jest.fn()
+          data: {
+            metaData: {},
+            resource: [
+              { id: 'P9DDPEV', name: 'cvng' },
+              { id: 'PU7R5AE', name: 'Sowmya' }
+            ],
+            responseMessages: []
+          },
+          refetch
         } as any)
     )
     const { container } = render(
-      <TestWrapper>
-        <PagerDutyChangeSource
-          formik={
-            {
-              values: {
-                spec: {
-                  pagerDutyServiceId: 'cvng',
-                  connectorRef: 'PagerDutyConnector'
-                }
-              }
-            } as any
+      <TestComponent
+        initialValues={{
+          spec: {
+            pagerDutyServiceId: 'P9DDPEV',
+            connectorRef: 'PagerDutyConnector'
           }
-          isEdit={true}
-        />
-      </TestWrapper>
+        }}
+      />
     )
 
+    await waitFor(() => expect(refetch).toHaveBeenCalled())
+    await waitFor(() => expect(container.querySelector('input[value="cvng"]')).toBeTruthy())
     await waitFor(() => expect(container.querySelector('.connectorField .bp3-disabled')).toBeDisabled())
+
+    // renders both service value in dropdown
+    act(() => {
+      fireEvent.click(container.querySelector(`.bp3-input-action [data-icon="chevron-down"]`)!)
+    })
+    const menuItemLabels = container.querySelectorAll('[class*="menuItemLabel"]')
+    await waitFor(() => expect(menuItemLabels.length).toEqual(2))
+    await waitFor(() => expect(menuItemLabels[0].innerHTML).toEqual('cvng'))
+    await waitFor(() => expect(menuItemLabels[1].innerHTML).toEqual('Sowmya'))
   })
 })
