b7a65b3c7537c585525acbeb1931edb8670f6609
Kevin Nagurski <knagurski@gmail.com>
2021-12-01 16:35:07 +0000

    feat: [FFM-1847]: Update Flag Configuration step type (#6311)

--- src/modules/70-pipeline/components/PipelineSteps/PipelineStepInterface.ts
+++ src/modules/70-pipeline/components/PipelineSteps/PipelineStepInterface.ts
@@ -45,6 +45,6 @@ export enum StepType {
   InfraProvisioning = 'InfraProvisioning',
   KubernetesGcp = 'KubernetesGcp',
   ResourceConstraint = 'ResourceConstraint',
-  FlagConfiguration = 'FLAG_CONFIGURATION',
+  FlagConfiguration = 'FlagConfiguration',
   Template = 'Template'
 }
