989eb9d507abcffcf2c6e2dc4138621c4c0f5969
Aleksandar Zivkovic <aleksandar.zivkovic@harness.io>
2022-01-11 09:37:18 +0100

    fix: [DEL-2856]: del selector spacing issue (#6802)
    * fix: [DEL-2856]: del selector spacing issue
    
    * fix: [DEL-2856]: snapshot update

--- src/modules/10-common/components/DelegateSelectors/DelegateSelectors.module.scss.d.ts
+++ src/modules/10-common/components/DelegateSelectors/DelegateSelectors.module.scss.d.ts
@@ -4,5 +4,6 @@ declare const styles: {
   readonly wrapper: string
   readonly loader: string
   readonly delegatePopover: string
+  readonly delegateInput: string
 }
 export default styles
--- src/modules/10-common/components/DelegateSelectors/DelegateSelectors.tsx
+++ src/modules/10-common/components/DelegateSelectors/DelegateSelectors.tsx
@@ -134,6 +134,7 @@ export const DelegateSelectors = (props: DelegateSelectorsProps): React.ReactEle
             return validTag && validExpression
           }}
           placeholder={placeholder || getString('delegate.Delegate_Selector_placeholder')}
+          className={css.delegateInput}
         />
       )}
     </div>
