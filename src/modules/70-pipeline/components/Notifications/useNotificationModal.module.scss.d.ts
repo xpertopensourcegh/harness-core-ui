6482b30a8aed7cdfeaeaba0bdc878451c30f4d9c
satyam-harness <96036463+satyam-harness@users.noreply.github.com>
2022-01-19 22:27:26 +0530

    fix: [PIE-2078]: while creating new notification, in pipeline step ui was breaking (#6921)
    * fix: while creating new notification, in pipeline step ui was breaking
    
    * fix: snapshot updated for FilterCRUD.test.tsx
    
    * fix: testcases updated
    
    * fix: snapshot updated
    
    * Update useCDTrialModal.test.tsx.snap
    
    * Update FilterCRUD.test.tsx.snap
    
    Co-authored-by: Kapil Kumawat <kapil.kumawat@harness.io>

--- src/modules/70-pipeline/components/Notifications/useNotificationModal.module.scss.d.ts
+++ src/modules/70-pipeline/components/Notifications/useNotificationModal.module.scss.d.ts
@@ -1,17 +1,18 @@
-/* eslint-disable */
-/**
- * Copyright 2021 Harness Inc. All rights reserved.
+/*
+ * Copyright 2022 Harness Inc. All rights reserved.
  * Use of this source code is governed by the PolyForm Shield 1.0.0 license
  * that can be found in the licenses directory at the root of this repository, also available at
  * https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt.
- **/
-// this is an auto-generated file, do not update this manually
+ */
+
+/* eslint-disable */
+// this is an auto-generated file
 declare const styles: {
+  readonly formContent: string
   readonly crossIcon: string
   readonly dialog: string
   readonly forStages: string
-  readonly formContent: string
-  readonly formError: string
   readonly stagesMultiSelect: string
+  readonly formError: string
 }
 export default styles
