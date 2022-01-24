ce7be985cc1487dd972d03e4f6d12992f0f520eb
Vardan Bansal <vardan.bansal@harness.io>
2022-01-24 18:17:34 +0530

    fix: [CI-2698]: Add missing tooltip on CI Codebase label (#6986)

--- src/modules/70-pipeline/components/PipelineInputSetForm/PipelineInputSetForm.tsx
+++ src/modules/70-pipeline/components/PipelineInputSetForm/PipelineInputSetForm.tsx
@@ -6,16 +6,7 @@
  */
 
 import React from 'react'
-import {
-  Layout,
-  getMultiTypeFromValue,
-  MultiTypeInputType,
-  Text,
-  Icon,
-  Color,
-  IconName,
-  HarnessDocTooltip
-} from '@wings-software/uicore'
+import { Layout, getMultiTypeFromValue, MultiTypeInputType, Text, Icon, Color, IconName } from '@wings-software/uicore'
 import { isEmpty, get, defaultTo } from 'lodash-es'
 import cx from 'classnames'
 import { useParams } from 'react-router-dom'
@@ -251,9 +242,13 @@ const PipelineInputSetFormInternal: React.FC<PipelineInputSetFormProps> = props 
             MultiTypeInputType.RUNTIME && (
             <>
               <Layout.Horizontal spacing="small" padding={{ top: 'medium', left: 'large', right: 0, bottom: 0 }}>
-                <Text data-name="ci-codebase-title" color={Color.BLACK_100} font={{ weight: 'semi-bold' }}>
+                <Text
+                  data-name="ci-codebase-title"
+                  color={Color.BLACK_100}
+                  font={{ weight: 'semi-bold' }}
+                  tooltipProps={{ dataTooltipId: 'ciCodebase' }}
+                >
                   {getString('ciCodebase')}
-                  <HarnessDocTooltip tooltipId="ciCodebaseTitle" useStandAlone={true} />
                 </Text>
               </Layout.Horizontal>
               <div className={css.topAccordion}>
