6f1c672e1f336007e10ea8dec6c6bbc59c99b1ab
sunnykesh-harness <sunnykesh.shandilya@harness.io>
2022-02-08 16:32:20 +0530

    feat: [PL-22399]: Made a component for SCM validation and info (#7212)
    * feat: [PL-22399]: made a component for SCM warning and user profile info for git commits
    
    * feat: [PL-22399]: Support for consumer to validate SCM
    
    * feat: [PL-22399]: Adding tests for SCMCheck
    
    * feat: [PL-22399]: Addressed review commments

---
+++ src/modules/10-common/components/SCMCheck/SCMCheck.tsx
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2022 Harness Inc. All rights reserved.
+ * Use of this source code is governed by the PolyForm Shield 1.0.0 license
+ * that can be found in the licenses directory at the root of this repository, also available at
+ * https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt.
+ */
+
+import React, { useEffect } from 'react'
+import { Link, useParams } from 'react-router-dom'
+import { Avatar, Color, Container, FontVariation, Icon, Layout, Text } from '@harness/uicore'
+import { noop, defaultTo } from 'lodash-es'
+import cx from 'classnames'
+import { useAppStore } from 'framework/AppStore/AppStoreContext'
+import { useGitSyncStore } from 'framework/GitRepoStore/GitSyncStoreContext'
+import paths from '@common/RouteDefinitions'
+import { useStrings } from 'framework/strings/String'
+import type { AccountPathProps } from '@common/interfaces/RouteInterfaces'
+import type { UserInfo } from 'services/cd-ng'
+import css from './SCMCheck.module.scss'
+
+export interface SCMCheckProps {
+  profileLinkClickHandler?: () => void // If used inside modal, this can be use to close modal
+  title?: string
+  className?: string
+  validateSCM?: (hasValidSCM: boolean) => void // Can be used to disable the commit
+}
+
+const SCMCheck: React.FC<SCMCheckProps> = props => {
+  const { currentUserInfo: currentLoggedInUser } = useAppStore()
+  const { codeManagers, loadingCodeManagers } = useGitSyncStore()
+  const { accountId } = useParams<AccountPathProps>()
+  const [currentUserInfo, setCurrentUserInfo] = React.useState('')
+  const { getString } = useStrings()
+  const { validateSCM = noop, profileLinkClickHandler = noop, className = '', title } = props
+
+  const getUserDisplayName = (userInfo: UserInfo): string => {
+    return defaultTo(userInfo?.name, userInfo?.email) || getString('na')
+  }
+
+  useEffect(() => {
+    const userSpec = codeManagers?.[0]?.authentication?.spec?.spec
+    const user = userSpec?.username || userSpec?.usernameRef
+
+    setCurrentUserInfo(user)
+    validateSCM(!loadingCodeManagers && user)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [codeManagers])
+
+  return (
+    <Container className={cx(className)} margin={{ bottom: 'large' }}>
+      {currentUserInfo ? (
+        <Layout.Horizontal className={css.userInfo} flex={{ alignItems: 'center' }}>
+          <Avatar
+            size="small"
+            name={getUserDisplayName(currentLoggedInUser)}
+            backgroundColor={Color.PRIMARY_7}
+            hoverCard={false}
+          />
+          <Text color={Color.GREY_700}>
+            {getString('common.git.currentUserLabel', { user: getUserDisplayName(currentLoggedInUser) })}
+          </Text>
+        </Layout.Horizontal>
+      ) : (
+        <></>
+      )}
+      {title ? (
+        <Text font={{ variation: FontVariation.H3 }} margin={{ bottom: 'large', top: 'small' }}>
+          {title}
+        </Text>
+      ) : (
+        <></>
+      )}
+
+      {!loadingCodeManagers && !currentUserInfo && (
+        <Layout.Horizontal className={css.addUserContainer} spacing="small">
+          <Icon name="warning-sign" color={Color.ORANGE_700}></Icon>
+          <div>
+            <Text font={{ variation: FontVariation.SMALL }} color={Color.BLACK}>
+              {getString('common.git.noUserLabel')}
+            </Text>
+            <Link to={paths.toUserProfile({ accountId })} target="_blank" onClick={profileLinkClickHandler}>
+              <Text inline font={{ variation: FontVariation.SMALL_BOLD }} color={Color.PRIMARY_7}>
+                {getString('common.git.addUserCredentialLabel')}
+              </Text>
+            </Link>
+          </div>
+        </Layout.Horizontal>
+      )}
+    </Container>
+  )
+}
+
+export default SCMCheck
---
+++ src/modules/10-common/components/SCMCheck/__tests__/SCMCheck.test.tsx
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2022 Harness Inc. All rights reserved.
+ * Use of this source code is governed by the PolyForm Shield 1.0.0 license
+ * that can be found in the licenses directory at the root of this repository, also available at
+ * https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt.
+ */
+
+import React from 'react'
+import { render, waitFor } from '@testing-library/react'
+import { noop } from 'lodash-es'
+import { GitSyncTestWrapper } from '@common/utils/gitSyncTestUtils'
+import SCMCheck from '../SCMCheck'
+
+const pathParams = { accountId: 'dummy', orgIdentifier: 'default', projectIdentifier: 'dummyProject' }
+const title = 'Save to git'
+const handlerForSCM = jest.fn()
+
+describe('Test SCMCheck', () => {
+  test('SCMCheck while loading should render none of user info or missing SCM warning', async () => {
+    const { container, getByText } = render(
+      <GitSyncTestWrapper
+        path="/account/:accountId/ci/orgs/:orgIdentifier/projects/:projectIdentifier/admin/git-sync/repos"
+        pathParams={pathParams}
+        gitSyncStoreValues={{ loadingCodeManagers: true, codeManagers: [] }}
+      >
+        <SCMCheck title={title} validateSCM={noop} />
+      </GitSyncTestWrapper>
+    )
+    expect(container).toMatchSnapshot()
+    await waitFor(() => {
+      expect(getByText(title)).toBeInTheDocument()
+    })
+  })
+
+  test('SCMCheck should render currentUserLabel if SCM is present', async () => {
+    const { container, getByText } = render(
+      <GitSyncTestWrapper
+        path="/account/:accountId/ci/orgs/:orgIdentifier/projects/:projectIdentifier/admin/git-sync/repos"
+        pathParams={pathParams}
+        defaultAppStoreValues={{ currentUserInfo: { name: 'dev', email: 'mail@harness.io' } }}
+      >
+        <SCMCheck title={title} validateSCM={handlerForSCM} />
+      </GitSyncTestWrapper>
+    )
+    expect(container).toMatchSnapshot()
+    await waitFor(() => {
+      expect(getByText('common.git.currentUserLabel')).toBeInTheDocument()
+    })
+    expect(handlerForSCM).toBeCalled()
+  })
+
+  test('SCMCheck should render currentUserLabel using email if name is not present', async () => {
+    const { container } = render(
+      <GitSyncTestWrapper
+        path="/account/:accountId/ci/orgs/:orgIdentifier/projects/:projectIdentifier/admin/git-sync/repos"
+        pathParams={pathParams}
+        defaultAppStoreValues={{ currentUserInfo: { email: 'mail@harness.io' } }}
+      >
+        <SCMCheck title={title} validateSCM={handlerForSCM} />
+      </GitSyncTestWrapper>
+    )
+    expect(container).toMatchSnapshot()
+  })
+
+  test('SCMCheck should render warning for missing SCM with link to addUserCredentialLabel', async () => {
+    const { container, getByText } = render(
+      <GitSyncTestWrapper
+        path="/account/:accountId/ci/orgs/:orgIdentifier/projects/:projectIdentifier/admin/git-sync/repos"
+        pathParams={pathParams}
+        gitSyncStoreValues={{ loadingCodeManagers: false, codeManagers: [] }}
+      >
+        <SCMCheck title={title} validateSCM={handlerForSCM} />
+      </GitSyncTestWrapper>
+    )
+    expect(container).toMatchSnapshot()
+    await waitFor(() => {
+      expect(getByText('common.git.noUserLabel')).toBeInTheDocument()
+      expect(getByText('common.git.addUserCredentialLabel')).toBeInTheDocument()
+    })
+    expect(handlerForSCM).toBeCalled()
+  })
+})
