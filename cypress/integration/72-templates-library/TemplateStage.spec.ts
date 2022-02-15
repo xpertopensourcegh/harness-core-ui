77d3a01eae3671e3d46219365a7925d3e7e5722d
Samarth Sharma <96409598+samarth-sharma31@users.noreply.github.com>
2022-02-15 12:01:23 +0530

    fix: [CDS-25770]: adding cypress coverage (#7320)

---
+++ cypress/integration/72-templates-library/TemplateStage.spec.ts
@@ -0,0 +1,68 @@
+
+describe('Template Stage Selection', () => {
+
+    const gitSyncEnabledCall =
+        '/ng/api/git-sync/git-sync-enabled?accountIdentifier=accountId&orgIdentifier=default&projectIdentifier=project1'
+    const templatesListCall =
+        '/template/api/templates/list?routingId=accountId&accountIdentifier=accountId&orgIdentifier=default&projectIdentifier=project1&templateListType=Stable&searchTerm=&page=0&size=20&includeAllTemplatesAvailableAtScope=true'
+    const pipelineVariablesCall =
+        '/pipeline/api/pipelines/variables?routingId=accountId&accountIdentifier=accountId&orgIdentifier=default&projectIdentifier=project1'
+    const templateDetailsCall =
+        '/template/api/templates/Cypress_Template_Example_1?routingId=accountId&accountIdentifier=accountId&projectIdentifier=project1&orgIdentifier=default&versionLabel=Version1&getDefaultFromOtherRepo=true'
+
+    describe('Tests template stage configuration', () => {
+        beforeEach(() => {
+            cy.on('uncaught:exception', () => {
+                // returning false here prevents Cypress from
+                // failing the test
+                return false
+            })
+            cy.intercept('GET', gitSyncEnabledCall, { connectivityMode: null, gitSyncEnabled: false })
+            cy.intercept('POST', templatesListCall, { fixture: 'template/api/templatesList' })
+            cy.login('test', 'test')
+
+            cy.visitCreatePipeline()
+
+            cy.fillName('testPipeline_Cypress')
+
+            cy.clickSubmit()
+
+            cy.get('[icon="plus"]').click()
+
+            cy.get('[data-icon="template-library"]').click()
+        })
+
+        it('test template stage selection and configuration', () => {
+            cy.intercept('POST', pipelineVariablesCall, { fixture: 'template/api/pipelines.variables' })
+            cy.intercept('GET', templateDetailsCall, { fixture: 'template/api/templateDetails' })
+            cy.wait(1000)
+
+            cy.contains('p', 'Cypress Template Example 1').should('be.visible')
+            cy.contains('p', 'Cypress_Template_Example_1').should('be.visible')
+            cy.contains('p', 'Cypress Template Example 1').click()
+
+            cy.wait(1000)
+            cy.contains('span', 'Use Template').click()
+
+            cy.fillField('name', 'cypress stage')
+            cy.clickSubmit()
+
+            cy.contains('span', 'Stage Name').should('be.visible')
+
+            cy.get('input[name="name"]').should('have.value', 'cypress stage')
+
+            cy.get('[data-icon="more"]').click()
+
+            cy.contains('p', 'Change Template').should('be.visible')
+            cy.contains('p', 'Remove Template').should('be.visible')
+            cy.contains('p', 'Open Template in new tab').should('be.visible')
+            cy.contains('p', 'Preview Template YAML').should('be.visible')
+
+            cy.contains('p', 'Preview Template YAML').click()
+
+            cy.contains('h4', 'template.yaml').should('be.visible')
+            cy.contains('span', 'Cypress Template Example 1').should('be.visible')
+            cy.contains('span', 'Cypress_Template_Example_1').should('be.visible')
+        })
+    })
+})
