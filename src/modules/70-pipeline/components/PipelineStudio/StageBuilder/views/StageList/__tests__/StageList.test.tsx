c239e3c305b866e091018dcbae5a9337adf58427
shamala-harness <51908429+shamala-harness@users.noreply.github.com>
2022-02-21 16:23:07 +0530

    feat: [CDS-29136]: Added test case for StageList file (#7403)
    * feat: [CDS-29136]: Added test case for StageList file
    
    * feat: [CDS-29136]: Added test case for StageList file

--- src/modules/70-pipeline/components/PipelineStudio/StageBuilder/views/StageList/__tests__/StageList.test.tsx
+++ src/modules/70-pipeline/components/PipelineStudio/StageBuilder/views/StageList/__tests__/StageList.test.tsx
@@ -6,31 +6,41 @@
  */
 
 import React from 'react'
-import { render } from '@testing-library/react'
+import { act, fireEvent, render } from '@testing-library/react'
 import type { IconName } from '@harness/uicore'
 import { StageType } from '@pipeline/utils/stageHelpers'
 import { StageList } from '../StageList'
-
+const baseProps = {
+  stages: [
+    { stage: { name: 'Stage 1', identifier: 'Stage_1', type: 'Deployment', spec: {} } },
+    { stage: { name: 'Stage 2', identifier: 'Stage_2', template: { templateRef: 'Stage_Template' } } }
+  ],
+  stagesMap: {
+    Deployment: {
+      name: 'Deploy',
+      type: StageType.DEPLOY,
+      icon: 'cd-main' as IconName,
+      iconColor: 'var(--pipeline-deploy-stage-color)',
+      isApproval: false,
+      openExecutionStrategy: true
+    }
+  },
+  templateTypes: { Stage_Template: 'Deployment' }
+}
 describe('<StageList /> tests', () => {
   test('snapshot test', async () => {
-    const baseProps = {
-      stages: [
-        { stage: { name: 'Stage 1', identifier: 'Stage_1', type: 'Deployment', spec: {} } },
-        { stage: { name: 'Stage 2', identifier: 'Stage_2', template: { templateRef: 'Stage_Template' } } }
-      ],
-      stagesMap: {
-        Deployment: {
-          name: 'Deploy',
-          type: StageType.DEPLOY,
-          icon: 'cd-main' as IconName,
-          iconColor: 'var(--pipeline-deploy-stage-color)',
-          isApproval: false,
-          openExecutionStrategy: true
-        }
-      },
-      templateTypes: { Stage_Template: 'Deployment' }
-    }
     const { container } = render(<StageList {...baseProps} />)
     expect(container).toMatchSnapshot()
+  })
+  test('Test Add selected stage at the start- Stage_2 before Stage_1', async () => {
+    const props = { ...baseProps, selectedStageId: 'Stage_2', onClick: jest.fn() }
+    const { container } = render(<StageList {...props} />)
+    expect(container).toMatchSnapshot()
+    // Test onClick from props should be invoked
+    const element = document.getElementsByClassName('Layout--horizontal')
+    const stage2 = element[0]
+    act(() => {
+      fireEvent.click(stage2)
+    })
   })
 })
