059fb976195dba91cebd56ac9b17218a95368423
Vivek Kumar Bansal <5930351+vkbansal@users.noreply.github.com>
2022-02-25 12:57:14 +0530

    chore: [PIE-2834]: upgrade storybook (#7527)
    * chore: [PIE-2834]: upgrade storybook
    
    * chore: [PIE-2834]: upgrade storybook
    
    * chore: [PIE-2834]: upgrade storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,11 +5,12 @@
  * https://polyformproject.org/wp-content/uploads/2020/06/PolyForm-Shield-1.0.0.txt.
  */
 
-const webpackConfig = require('../webpack.config')
+const webpackConfig = require('../configs/webpack.dev')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const MonacoWebpackPlugin = require(require.resolve('monaco-editor-webpack-plugin', { paths: [process.cwd()] }))
 const path = require('path')
-
+const { merge, mergeWithRules } = require('webpack-merge')
+const webpack = require('webpack')
 const CONTEXT = process.cwd()
 
 module.exports = {
@@ -19,118 +20,39 @@ module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   webpackFinal: config => {
-    return {
-      ...config,
-      cache: { type: 'filesystem', name: 'storybook' },
+    const c1 = mergeWithRules({
       module: {
-        ...config.module,
+        rules: {
+          test: 'match',
+          use: 'replace'
+        }
+      }
+    })(config, {
+      // cache: { type: 'filesystem', name: 'storybook' },
+      devtool: false,
+      module: {
         rules: [
           {
-            test: /.*\/node_modules\/xterm\/.*\.css/,
-            use: 'null-loader'
-          },
-          {
-            test: /.*\/node_modules\/monaco-editor\/.*/,
-            use: 'null-loader'
-          },
-          {
-            test: /.*\/node_modules\/@harness\/monaco-yaml\/.*/,
-            use: 'null-loader'
-          },
-          ...config.module.rules,
-          {
-            test: /\.m?js$/,
-            include: /node_modules/,
-            type: 'javascript/auto'
-          },
-          {
-            test: /\.(j|t)sx?$/,
+            test: /\.(mjs|tsx?|jsx?)$/,
             exclude: /node_modules/,
             use: [
               {
-                loader: 'ts-loader',
-                options: {
-                  transpileOnly: true
-                }
+                loader: 'swc-loader'
               }
             ]
           },
-          {
-            test: /\.module\.scss$/,
-            exclude: /node_modules/,
-            use: [
-              MiniCssExtractPlugin.loader,
-              {
-                loader: '@harness/css-types-loader',
-                options: {
-                  prettierConfig: CONTEXT
-                }
-              },
-              {
-                loader: 'css-loader',
-                options: {
-                  importLoaders: 1,
-                  modules: {
-                    mode: 'local',
-                    localIdentName: '[name]_[local]_[hash:base64:6]',
-                    exportLocalsConvention: 'camelCaseOnly'
-                  }
-                }
-              },
-              {
-                loader: 'sass-loader',
-                options: {
-                  sassOptions: {
-                    includePaths: [path.join(CONTEXT, 'src')]
-                  },
-                  sourceMap: false,
-                  implementation: require('sass')
-                }
-              }
-            ]
-          },
-          {
-            test: /(?<!\.module)\.scss$/,
-            exclude: /node_modules/,
-            use: [
-              MiniCssExtractPlugin.loader,
-              {
-                loader: 'css-loader',
-                options: {
-                  importLoaders: 1,
-                  modules: false
-                }
-              },
-              {
-                loader: 'sass-loader',
-                options: {
-                  sassOptions: {
-                    includePaths: [path.join(CONTEXT, 'src')]
-                  },
-                  implementation: require('sass')
-                }
-              }
-            ]
-          },
-          {
-            test: /\.ya?ml$/,
-            type: 'json',
-            use: [
-              {
-                loader: 'yaml-loader'
-              }
-            ]
-          }
-          // ...webpackConfig.module.rules
+          ...webpackConfig.module.rules
         ]
       },
       resolve: {
-        ...config.resolve,
         ...webpackConfig.resolve,
-        fallback: { assert: require.resolve('assert/') }
+        alias: {
+          ...webpackConfig.resolve.alias,
+          'monaco-editor/.*': false,
+          '@harness/monaco-yaml/.*': false
+        }
       },
       plugins: [
-        ...config.plugins,
         new MiniCssExtractPlugin({
           filename: 'static/[name].css',
           chunkFilename: 'static/[name].[id].css'
@@ -138,9 +60,13 @@ module.exports = {
         new MonacoWebpackPlugin({
           // available options are documented at https://github.com/Microsoft/monaco-editor-webpack-plugin#options
           languages: ['yaml', 'shell', 'powershell']
+        }),
+        new webpack.WatchIgnorePlugin({
+          paths: [/node_modules(?!\/@harness)/, /\.(d|test)\.tsx?$/, /stringTypes\.ts/, /\.snap$/]
         })
-      ],
-      stats: 'minimal'
-    }
+      ]
+    })
+
+    return c1
   }
 }
