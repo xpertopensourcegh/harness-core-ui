9cd9d7a3c2a8838108a5165dafa48228af315f7f
Shashidhar Gani <shashidhar.gani@harness.io>
2022-02-25 17:00:50 +0530

    feat: [CVS-9157]: Added cypress for Prometheus (#7443)
    * feat: [CVS-9157]: Added cypress for Prometheus
    
    * feat: [CVS-9157]: Review changes
    
    * feat: [CVS-9157]: Added 3 more form validations

---
+++ cypress/integration/85-cv/HealthSource/Prometheus.spec.ts
@@ -0,0 +1,259 @@
+import {
+  monitoredServiceListCall,
+  monitoredServiceListResponse
+} from '../../../support/85-cv/monitoredService/constants'
+import {
+  labelNamesAPI,
+  labelNamesResponse,
+  metricListAPI,
+  metricListResponse,
+  sampleDataAPI,
+  sampleDataResponse,
+  metricPackAPI,
+  metricPackResponse,
+  monitoredService,
+  labelValuesAPI,
+  monitoredServiceForBuildQuery
+} from '../../../support/85-cv/monitoredService/health-sources/Prometheus/constant'
+import { errorResponse } from '../../../support/85-cv/slos/constants'
+import { Connectors } from '../../../utils/connctors-utils'
+
+describe('Health Source - Prometheus', () => {
+  beforeEach(() => {
+    cy.on('uncaught:exception', () => {
+      return false
+    })
+    cy.login('test', 'test')
+    cy.intercept('GET', monitoredServiceListCall, monitoredServiceListResponse)
+    cy.intercept(
+      'GET',
+      '/cv/api/monitored-service/count-of-services?routingId=accountId&accountId=accountId&orgIdentifier=default&projectIdentifier=project1',
+      { allServicesCount: 1, servicesAtRiskCount: 0 }
+    )
+    cy.visitChangeIntelligence()
+  })
+
+  it('should be able to add Prometheus Health Source with manual query', () => {
+    cy.addNewMonitoredServiceWithServiceAndEnv()
+    cy.populateDefineHealthSource(Connectors.PROMETHEUS, 'prometheus-sale', 'Prometheus')
+
+    cy.get('input[name="product"]').should('be.disabled')
+
+    cy.intercept('GET', metricPackAPI, metricPackResponse)
+    cy.intercept('GET', labelNamesAPI, labelNamesResponse)
+    cy.intercept('GET', metricListAPI, metricListResponse)
+
+    cy.findByRole('button', { name: /Next/i }).click()
+
+    cy.contains('h2', 'Query Specifications and Mapping').should('be.visible')
+
+    cy.get('input[name="metricName"]').should('contain.value', 'Prometheus Metric')
+    cy.get('input[name="metricName"]').clear()
+
+    cy.contains('span', 'Metric Name is required.').should('be.visible')
+    cy.fillField('metricName', 'Prometheus Metric')
+    cy.contains('span', 'Metric Name is required.').should('not.exist')
+
+    cy.findByRole('button', { name: /Submit/i }).click()
+    cy.contains('span', 'Group Name is required.').should('be.visible')
+    cy.addingGroupName('Group 1')
+    cy.get('input[name="groupName"]').should('contain.value', 'Group 1')
+    cy.contains('span', 'Group Name is required.').should('not.exist')
+
+    cy.contains('div', 'Build your Query').should('be.visible')
+    cy.get('button span[data-icon="Edit"]').click()
+    cy.contains('p', 'Query Builder will not be available').should('be.visible')
+    cy.findByRole('button', { name: /Proceed to Edit/i }).click()
+
+    cy.contains('p', 'Query Builder will not be available since this mapping contains a manually edited query.').should(
+      'be.visible'
+    )
+    cy.contains('div', 'Build your Query').should('not.exist')
+
+    cy.get('textarea[name="query"]').focus().blur()
+    cy.contains('span', 'Query is required.').should('be.visible')
+    cy.findByRole('button', { name: /Fetch records/i }).should('be.disabled')
+    cy.get('textarea[name="query"]').type(`classes	{}`)
+    cy.contains('span', 'Query is required.').should('not.exist')
+
+    cy.contains('p', 'Submit query to see records from Prometheus').should('be.visible')
+
+    cy.intercept('GET', sampleDataAPI, errorResponse)
+
+    cy.findByRole('button', { name: /Fetch records/i }).click()
+
+    cy.contains('p', 'We cannot perform your request at the moment. Please try again.').should('exist')
+
+    cy.intercept('GET', sampleDataAPI, { statusCode: 200, body: {} })
+
+    cy.findByRole('button', { name: /Retry/i }).click()
+
+    cy.contains('p', 'No records for provided query').should('be.visible')
+
+    cy.intercept('GET', sampleDataAPI, sampleDataResponse)
+
+    cy.findByRole('button', { name: /Fetch records/i }).click()
+
+    cy.contains('div', 'Assign').click()
+    cy.findByRole('button', { name: /Submit/i }).click()
+
+    cy.contains('span', 'One selection is required.').should('be.visible')
+    cy.get('input[name="sli"]').click({ force: true })
+    cy.contains('span', 'One selection is required.').should('not.exist')
+
+    cy.get('input[name="continuousVerification"]').click({ force: true })
+    cy.get('input[name="healthScore"]').click({ force: true })
+
+    cy.contains('span', 'Risk Category is required.').should('exist')
+    cy.contains('label', 'Errors').click()
+    cy.contains('span', 'Risk Category is required.').should('not.exist')
+
+    cy.contains('span', 'Deviation Compared to Baseline is required.').should('exist')
+    cy.get('input[name="higherBaselineDeviation"]').click({ force: true })
+    cy.contains('span', 'Deviation Compared to Baseline is required.').should('not.exist')
+
+    cy.get('input[name="serviceInstance"]').click()
+    cy.contains('p', '__name__').click()
+
+    cy.findByRole('button', { name: /Add Metric/i }).click()
+
+    cy.contains('div', 'Map Metric(s) to Harness Services').click()
+
+    cy.fillField('metricName', 'Prometheus Metric')
+
+    cy.contains('span', 'Metric name must be unique.').should('be.visible')
+    cy.fillField('metricName', 'Prometheus Metric 123')
+    cy.contains('span', 'Metric name must be unique.').should('not.exist')
+
+    cy.get('input[name="groupName"]').click()
+    cy.contains('p', 'Group 1').click()
+
+    cy.get('button span[data-icon="Edit"]').click()
+    cy.contains('p', 'Query Builder will not be available').should('be.visible')
+    cy.findByRole('button', { name: /Proceed to Edit/i }).click()
+    cy.get('textarea[name="query"]').type(`classes	{}`)
+
+    cy.contains('div', 'Assign').click()
+    cy.get('input[name="sli"]').click({ force: true })
+
+    cy.findByRole('button', { name: /Submit/i }).click({ force: true })
+  })
+
+  it('should be able to edit an existing Prometheus health source of manual query', () => {
+    cy.intercept('GET', '/cv/api/monitored-service/service1_env1?*', monitoredService)
+
+    cy.wait(2000)
+
+    cy.get('span[data-icon="Options"]').click()
+    cy.contains('div', 'Edit service').click()
+
+    cy.contains('div', 'Prometheus').click()
+
+    cy.intercept('GET', metricPackAPI, metricPackResponse)
+    cy.intercept('GET', labelNamesAPI, labelNamesResponse)
+    cy.intercept('GET', metricListAPI, metricListResponse)
+
+    cy.findByRole('button', { name: /Next/i }).click()
+    cy.findByRole('button', { name: /Submit/i }).click()
+    cy.findByRole('button', { name: /Save/i }).click()
+    cy.contains('span', 'Monitored Service updated').should('be.visible')
+  })
+
+  it('should be able to add Prometheus HS by building a query', () => {
+    cy.addNewMonitoredServiceWithServiceAndEnv()
+    cy.populateDefineHealthSource(Connectors.PROMETHEUS, 'prometheus-sale', 'Prometheus')
+
+    cy.get('input[name="product"]').should('be.disabled')
+
+    cy.intercept('GET', metricPackAPI, metricPackResponse)
+    cy.intercept('GET', labelNamesAPI, labelNamesResponse)
+    cy.intercept('GET', metricListAPI, metricListResponse)
+
+    cy.findByRole('button', { name: /Next/i }).click()
+
+    cy.contains('h2', 'Query Specifications and Mapping').should('be.visible')
+
+    cy.addingGroupName('Group 1')
+    cy.get('input[name="groupName"]').should('contain.value', 'Group 1')
+
+    cy.contains('div', 'Build your Query').should('be.visible')
+    cy.get('button span[data-icon="Edit"]').click()
+    cy.contains('p', 'Query Builder will not be available').should('be.visible')
+    cy.findByRole('button', { name: /Proceed to Edit/i }).click()
+
+    cy.contains('p', 'Query Builder will not be available since this mapping contains a manually edited query.').should(
+      'be.visible'
+    )
+    cy.contains('div', 'Build your Query').should('not.exist')
+
+    cy.contains('p', 'Undo manual query selection').click()
+
+    cy.contains('div', 'Build your Query').should('be.visible')
+    cy.contains('div', 'Build your Query').click()
+
+    cy.intercept('GET', sampleDataAPI, sampleDataResponse)
+
+    cy.findByRole('button', { name: /Submit/i }).click()
+
+    cy.contains('span', 'Prometheus Metric is required.').should('be.visible')
+    cy.get('input[name="prometheusMetric"]').click({ force: true })
+    cy.contains('p', 'classes').click()
+    cy.contains('span', 'Prometheus Metric is required.').should('not.exist')
+
+    cy.intercept('GET', labelValuesAPI, metricListResponse)
+
+    cy.contains('span', 'Filter on Environment is required.').should('be.visible')
+    cy.get('input[name="envFilter"]').click({ force: true })
+    cy.contains('p', '__name__').click()
+    cy.contains('p', 'classes').click()
+    cy.contains('span', 'Filter on Environment').click()
+    cy.contains('span', 'Filter on Environment is required.').should('not.exist')
+
+    cy.wait(1000)
+
+    cy.contains('span', 'Filter on Service required.').should('be.visible')
+    cy.get('input[name="serviceFilter"]').click({ force: true })
+    cy.contains('p', 'generation').click()
+    cy.contains('p', 'classes_loaded').click()
+    cy.contains('span', 'Filter on Service').click()
+    cy.contains('span', 'Filter on Service required.').should('not.exist')
+
+    cy.wait(1000)
+
+    cy.get('input[name="additionalFilter"]').click({ force: true })
+    cy.contains('p', 'group').click()
+    cy.contains('p', 'classes_unloaded').click()
+    cy.contains('span', 'Additional Filter').click()
+
+    cy.get('input[name="aggregator"]').click({ force: true })
+    cy.contains('p', 'avg (calculate the average over dimensions)').click()
+
+    cy.contains('div', 'Assign').click()
+    cy.get('input[name="sli"]').click({ force: true })
+
+    cy.findByRole('button', { name: /Submit/i }).click()
+    cy.findByRole('button', { name: /Save/i }).click()
+    cy.contains('span', 'Monitored Service created').should('be.visible')
+  })
+
+  it('should be able to edit an existing Prometheus health source of Build query', () => {
+    cy.intercept('GET', '/cv/api/monitored-service/service1_env1?*', monitoredServiceForBuildQuery)
+
+    cy.wait(2000)
+
+    cy.get('span[data-icon="Options"]').click()
+    cy.contains('div', 'Edit service').click()
+
+    cy.contains('div', 'Prometheus').click()
+
+    cy.intercept('GET', metricPackAPI, metricPackResponse)
+    cy.intercept('GET', labelNamesAPI, labelNamesResponse)
+    cy.intercept('GET', metricListAPI, metricListResponse)
+    cy.intercept('GET', sampleDataAPI, sampleDataResponse)
+
+    cy.findByRole('button', { name: /Next/i }).click()
+    cy.findByRole('button', { name: /Submit/i }).click()
+    cy.findByRole('button', { name: /Save/i }).click()
+    cy.contains('span', 'Monitored Service updated').should('be.visible')
+  })
+})
---
+++ cypress/support/85-cv/monitoredService/health-sources/Prometheus/constant.ts
@@ -0,0 +1,427 @@
+const accountId = 'accountId'
+const orgIdentifier = 'default'
+const projectIdentifier = 'project1'
+const connectorIdentifier = 'prometheussale'
+const dataSourceType = 'PROMETHEUS'
+
+export const sampleDataAPI = `/cv/api/prometheus/sample-data?routingId=${accountId}&accountId=${accountId}&projectIdentifier=${projectIdentifier}&orgIdentifier=${orgIdentifier}&query=*&tracingId=*&connectorIdentifier=${connectorIdentifier}`
+
+export const sampleDataResponse = {
+  status: 'SUCCESS',
+  data: [
+    {
+      metricDetails: {
+        instance: 'prometheusgoto.cie-demo.co.uk:80',
+        pod: 'payment-service-goto-deployment-canary-67cdbc674b-bzcfs',
+        __name__: 'classes',
+        datalnddev: 'classes',
+        job: 'payment-service-nikpapag',
+        group: 'goto'
+      },
+      data: [
+        {
+          timestamp: 1645071060,
+          value: 16375
+        },
+        {
+          timestamp: 1645071120,
+          value: 16375
+        },
+        {
+          timestamp: 1645071180,
+          value: 16375
+        },
+        {
+          timestamp: 1645071240,
+          value: 16375
+        }
+      ]
+    }
+  ],
+  metaData: null,
+  correlationId: 'e73f74cd-2dec-49eb-9ea2-cfdf43961ed3'
+}
+
+export const metricPackAPI = `/cv/api/metric-pack?routingId=${accountId}&projectIdentifier=${projectIdentifier}&orgIdentifier=${orgIdentifier}&accountId=${accountId}&dataSourceType=${dataSourceType}`
+
+export const metricPackResponse = {
+  metaData: {},
+  resource: [
+    {
+      uuid: 'f3VK7oDoSXaVJmxZAEPHVQ',
+      accountId,
+      orgIdentifier,
+      projectIdentifier,
+      dataSourceType,
+      identifier: 'Errors',
+      category: 'Errors',
+      metrics: [
+        {
+          name: 'Errors',
+          type: 'ERROR',
+          path: null,
+          validationPath: null,
+          responseJsonPath: null,
+          validationResponseJsonPath: null,
+          thresholds: [],
+          included: false
+        }
+      ],
+      thresholds: null
+    },
+    {
+      uuid: 'inNziIl5TtGFjAtu05qQFg',
+      accountId,
+      orgIdentifier,
+      projectIdentifier,
+      dataSourceType,
+      identifier: 'Infrastructure',
+      category: 'Infrastructure',
+      metrics: [
+        {
+          name: 'Infrastructure',
+          type: 'INFRA',
+          path: null,
+          validationPath: null,
+          responseJsonPath: null,
+          validationResponseJsonPath: null,
+          thresholds: [],
+          included: false
+        }
+      ],
+      thresholds: null
+    },
+    {
+      uuid: 'sxlCCw3hSMWVFq9C3QYSlQ',
+      accountId,
+      orgIdentifier,
+      projectIdentifier,
+      dataSourceType,
+      identifier: 'Performance',
+      category: 'Performance',
+      metrics: [
+        {
+          name: 'Other',
+          type: 'ERROR',
+          path: null,
+          validationPath: null,
+          responseJsonPath: null,
+          validationResponseJsonPath: null,
+          thresholds: [],
+          included: false
+        },
+        {
+          name: 'Response Time',
+          type: 'RESP_TIME',
+          path: null,
+          validationPath: null,
+          responseJsonPath: null,
+          validationResponseJsonPath: null,
+          thresholds: [],
+          included: false
+        },
+        {
+          name: 'Throughput',
+          type: 'THROUGHPUT',
+          path: null,
+          validationPath: null,
+          responseJsonPath: null,
+          validationResponseJsonPath: null,
+          thresholds: [],
+          included: false
+        }
+      ],
+      thresholds: null
+    }
+  ],
+  responseMessages: []
+}
+
+export const labelNamesAPI = `/cv/api/prometheus/label-names?routingId=${accountId}&projectIdentifier=${projectIdentifier}&orgIdentifier=${orgIdentifier}&accountId=${accountId}&connectorIdentifier=${connectorIdentifier}&tracingId=*`
+
+export const labelNamesResponse = {
+  status: 'SUCCESS',
+  data: [
+    '__name__',
+    'datalnddev',
+    'generation',
+    'group',
+    'implementation',
+    'instance',
+    'job',
+    'major',
+    'minor',
+    'patchlevel',
+    'pod',
+    'version'
+  ],
+  metaData: null,
+  correlationId: '49e05f9e-2098-4efe-a7ee-c9b74083c436'
+}
+
+export const labelValuesAPI = `/cv/api/prometheus/label-values?routingId=${accountId}&projectIdentifier=${projectIdentifier}&orgIdentifier=${orgIdentifier}&accountId=${accountId}&connectorIdentifier=${connectorIdentifier}&labelName=*&tracingId=*`
+
+export const metricListAPI = `/cv/api/prometheus/metric-list?routingId=${accountId}&projectIdentifier=${projectIdentifier}&orgIdentifier=${orgIdentifier}&accountId=${accountId}&tracingId=*&connectorIdentifier=${connectorIdentifier}`
+
+export const metricListResponse = {
+  status: 'SUCCESS',
+  data: [
+    'classes',
+    'classes_loaded',
+    'classes_unloaded',
+    'datasource_primary_active',
+    'datasource_primary_usage',
+    'gauge_servo_response_application_metrics',
+    'gauge_servo_response_mvc_createpayment',
+    'gauge_servo_response_mvc_payment',
+    'gauge_servo_response_root',
+    'gauge_servo_response_star_star',
+    'gauge_servo_response_star_star_favicon_ico',
+    'gauge_servo_response_unmapped',
+    'gauge_servo_rest_max',
+    'gauge_servo_rest_min',
+    'gc_ps_marksweep_count',
+    'gc_ps_marksweep_time',
+    'gc_ps_scavenge_count',
+    'gc_ps_scavenge_time',
+    'heap',
+    'heap_committed',
+    'heap_init',
+    'heap_used',
+    'httpsessions_active',
+    'httpsessions_max',
+    'instance_uptime',
+    'mem',
+    'mem_free',
+    'nonheap',
+    'nonheap_committed',
+    'nonheap_init',
+    'nonheap_used',
+    'normalized_servo_rest_count',
+    'normalized_servo_rest_totaltime',
+    'process_cpu_seconds_total',
+    'process_max_fds',
+    'process_open_fds',
+    'process_resident_memory_bytes',
+    'process_start_time_seconds',
+    'process_virtual_memory_bytes',
+    'processors',
+    'python_gc_collections_total',
+    'python_gc_objects_collected_total',
+    'python_gc_objects_uncollectable_total',
+    'python_info',
+    'scrape_duration_seconds',
+    'scrape_samples_post_metric_relabeling',
+    'scrape_samples_scraped',
+    'scrape_series_added',
+    'systemload_average',
+    'threads',
+    'threads_daemon',
+    'threads_peak',
+    'threads_totalStarted',
+    'up',
+    'uptime'
+  ],
+  metaData: null,
+  correlationId: 'c9a53cb2-1a56-400c-ab74-edab8bce2307'
+}
+
+export const monitoredService = {
+  status: 'SUCCESS',
+  data: {
+    createdAt: 1642157779711,
+    lastModifiedAt: 1643278935584,
+    monitoredService: {
+      orgIdentifier,
+      projectIdentifier,
+      identifier: 'service1_env1',
+      name: 'service1_env1',
+      type: 'Application',
+      description: '',
+      serviceRef: 'service1',
+      environmentRef: 'env1',
+      environmentRefList: null,
+      tags: {},
+      sources: {
+        healthSources: [
+          {
+            name: 'Prometheus',
+            identifier: 'Prometheus',
+            type: 'Prometheus',
+            spec: {
+              connectorRef: connectorIdentifier,
+              metricDefinitions: [
+                {
+                  identifier: 'prometheus_metric',
+                  metricName: 'Prometheus Metric',
+                  riskProfile: {
+                    category: 'Errors',
+                    metricType: 'ERROR',
+                    thresholdTypes: ['ACT_WHEN_HIGHER']
+                  },
+                  analysis: {
+                    liveMonitoring: {
+                      enabled: true
+                    },
+                    deploymentVerification: {
+                      enabled: true,
+                      serviceInstanceFieldName: '__name__',
+                      serviceInstanceMetricPath: null
+                    },
+                    riskProfile: {
+                      category: 'Errors',
+                      metricType: 'ERROR',
+                      thresholdTypes: ['ACT_WHEN_HIGHER']
+                    }
+                  },
+                  sli: {
+                    enabled: true
+                  },
+                  query: 'classes {\n\n}',
+                  groupName: 'Group 1',
+                  serviceInstanceFieldName: '__name__',
+                  prometheusMetric: null,
+                  serviceFilter: null,
+                  envFilter: null,
+                  additionalFilters: null,
+                  aggregation: null,
+                  isManualQuery: true
+                },
+                {
+                  identifier: 'Prometheus_Metric_123',
+                  metricName: 'Prometheus Metric 123',
+                  riskProfile: {
+                    category: 'Errors',
+                    metricType: null,
+                    thresholdTypes: []
+                  },
+                  analysis: {
+                    liveMonitoring: {
+                      enabled: false
+                    },
+                    deploymentVerification: {
+                      enabled: false,
+                      serviceInstanceFieldName: null,
+                      serviceInstanceMetricPath: null
+                    },
+                    riskProfile: {
+                      category: 'Errors',
+                      metricType: null,
+                      thresholdTypes: []
+                    }
+                  },
+                  sli: {
+                    enabled: true
+                  },
+                  query: 'classes {\n\n}',
+                  groupName: 'Group 1',
+                  serviceInstanceFieldName: null,
+                  prometheusMetric: null,
+                  serviceFilter: null,
+                  envFilter: null,
+                  additionalFilters: null,
+                  aggregation: null,
+                  isManualQuery: true
+                }
+              ]
+            }
+          }
+        ],
+        changeSources: []
+      },
+      dependencies: []
+    }
+  },
+  metaData: null,
+  correlationId: 'f3cdbd07-e92d-4162-854e-c6aac938d0c0'
+}
+
+export const monitoredServiceForBuildQuery = {
+  status: 'SUCCESS',
+  data: {
+    createdAt: 1642157779711,
+    lastModifiedAt: 1643278935584,
+    monitoredService: {
+      orgIdentifier,
+      projectIdentifier,
+      identifier: 'service1_env1',
+      name: 'service1_env1',
+      type: 'Application',
+      description: '',
+      serviceRef: 'service1',
+      environmentRef: 'env1',
+      environmentRefList: null,
+      tags: {},
+      sources: {
+        healthSources: [
+          {
+            name: 'Prometheus',
+            identifier: 'Prometheus',
+            type: 'Prometheus',
+            spec: {
+              connectorRef: connectorIdentifier,
+              metricDefinitions: [
+                {
+                  identifier: 'prometheus_metric',
+                  metricName: 'Prometheus Metric',
+                  riskProfile: {
+                    category: 'Errors',
+                    metricType: null,
+                    thresholdTypes: []
+                  },
+                  analysis: {
+                    liveMonitoring: {
+                      enabled: false
+                    },
+                    deploymentVerification: {
+                      enabled: false,
+                      serviceInstanceFieldName: null,
+                      serviceInstanceMetricPath: null
+                    },
+                    riskProfile: {
+                      category: 'Errors',
+                      metricType: null,
+                      thresholdTypes: []
+                    }
+                  },
+                  sli: {
+                    enabled: true
+                  },
+                  query:
+                    'avg(classes{__name__:classes="__name__",__name__:classes="__name__",__name__:classes="__name__"})',
+                  groupName: 'Group 1',
+                  serviceInstanceFieldName: null,
+                  prometheusMetric: 'classes',
+                  serviceFilter: [
+                    {
+                      labelName: '__name__:classes',
+                      labelValue: '__name__'
+                    }
+                  ],
+                  envFilter: [
+                    {
+                      labelName: '__name__:classes',
+                      labelValue: '__name__'
+                    }
+                  ],
+                  additionalFilters: [
+                    {
+                      labelName: '__name__:classes',
+                      labelValue: '__name__'
+                    }
+                  ],
+                  aggregation: 'avg',
+                  isManualQuery: false
+                }
+              ]
+            }
+          }
+        ],
+        changeSources: []
+      },
+      dependencies: []
+    }
+  },
+  metaData: null,
+  correlationId: 'f3cdbd07-e92d-4162-854e-c6aac938d0c0'
+}
