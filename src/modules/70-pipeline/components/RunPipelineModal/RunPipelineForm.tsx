e19e0ef2aed0731de9348a5b38c3661bd34bdc23
Kapil Kumawat <erkapil2006@gmail.com>
2022-03-25 10:50:16 +0530

    fix: [PIE-3077]: Revert the fix for PIE-3077 (#7986)

--- src/modules/70-pipeline/components/RunPipelineModal/RunPipelineForm.tsx
+++ src/modules/70-pipeline/components/RunPipelineModal/RunPipelineForm.tsx
@@ -66,7 +66,6 @@ import { useMutateAsGet, useQueryParams } from '@common/hooks'
 import { yamlStringify } from '@common/utils/YamlHelperMethods'
 import { PipelineActions } from '@common/constants/TrackingConstants'
 import { useTelemetry } from '@common/hooks/useTelemetry'
-import { sanitize } from '@common/utils/JSONUtils'
 import type { InputSetDTO } from '@pipeline/utils/types'
 import { useDeepCompareEffect } from '@common/hooks/useDeepCompareEffect'
 import { useGetYamlWithTemplateRefsResolved } from 'services/template-ng'
@@ -715,23 +714,7 @@ function RunPipelineFormBasic({
 
   const formRefDom = React.useRef<HTMLElement | undefined>()
   const handleValidation = async (values: Values): Promise<void> => {
-    // Sanitize any empty objects
-    const latestPipeline = sanitize(
-      mergeWith(currentPipeline, { pipeline: values as PipelineInfoConfig }, currentValue => {
-        // Return empty object if there exists no key in the current pipeline that matches the one in the values passed.
-        if (currentValue === undefined) {
-          return {}
-        } else {
-          return undefined
-        }
-      }),
-      {
-        removeEmptyArray: false,
-        removeEmptyString: false
-      }
-    ) as {
-      pipeline: PipelineInfoConfig
-    }
+    const latestPipeline = { ...currentPipeline, pipeline: values as PipelineInfoConfig }
     setCurrentPipeline(latestPipeline)
     const runPipelineFormErrors = await getFormErrors(latestPipeline, yamlTemplate, pipeline)
     // https://github.com/formium/formik/issues/1392
