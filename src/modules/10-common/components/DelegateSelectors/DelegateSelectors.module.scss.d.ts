b84c722242975b770da0535fa32475c958867666
Tamaraius <96610348+Tamaraius@users.noreply.github.com>
2022-04-06 17:09:57 +0200

    fix: [PIE-3273]: fix delegate selector width and height (#8178)
    * fix: add max height and overflow scroll for delegate selector
    
    * fix: leave previous styles
    
    * fix: max width of tag and form input
    
    * fix: return old styling and move added styling to delegateSelector level
    
    * fix: add missing bracket
    
    * fix: additional fixes
    
    * fix: update snapshots

--- src/modules/10-common/components/DelegateSelectors/DelegateSelectors.module.scss.d.ts
+++ src/modules/10-common/components/DelegateSelectors/DelegateSelectors.module.scss.d.ts
@@ -7,6 +7,7 @@
  **/
 // this is an auto-generated file, do not update this manually
 declare const styles: {
+  readonly delegateInput: string
   readonly delegatePopover: string
   readonly loader: string
   readonly wrapper: string
