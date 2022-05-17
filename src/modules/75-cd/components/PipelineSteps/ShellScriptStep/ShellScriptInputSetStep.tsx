1294fcf3e63e86840ad2f394b5176f85dd305ae2
Ishant Mehta <43949150+ishantmehtaharness@users.noreply.github.com>
2022-05-17 20:11:25 +0530

    fix: [PIE-3776]: Run Pipeline Form run button is disabled (#8898)

--- src/modules/75-cd/components/PipelineSteps/ShellScriptStep/ShellScriptInputSetStep.tsx
+++ src/modules/75-cd/components/PipelineSteps/ShellScriptStep/ShellScriptInputSetStep.tsx
@@ -6,8 +6,8 @@
  */
 
 import React from 'react'
-import { getMultiTypeFromValue, MultiTypeInputType, FormInput, FormikForm, Formik } from '@wings-software/uicore'
-import { isEmpty, get, isArray, noop } from 'lodash-es'
+import { getMultiTypeFromValue, MultiTypeInputType, FormInput, FormikForm } from '@wings-software/uicore'
+import { isEmpty, get, isArray } from 'lodash-es'
 import cx from 'classnames'
 
 import { FieldArray } from 'formik'
@@ -42,207 +42,205 @@ export default function ShellScriptInputSetStep(props: ShellScriptInputSetStepPr
   const prefix = isEmpty(path) ? '' : `${path}.`
 
   return (
-    <Formik formName={'shellScriptInputSetStep'} initialValues={{}} onSubmit={noop}>
-      <FormikForm>
-        {getMultiTypeFromValue(template?.timeout) === MultiTypeInputType.RUNTIME && (
-          <div className={cx(stepCss.formGroup, stepCss.sm)}>
-            <FormMultiTypeDurationField
-              multiTypeDurationProps={{
-                enableConfigureOptions: false,
-                allowableTypes,
-                expressions,
-                disabled: readonly
-              }}
-              label={getString('pipelineSteps.timeoutLabel')}
-              name={`${prefix}timeout`}
-              disabled={readonly}
-            />
-          </div>
-        )}
+    <FormikForm>
+      {getMultiTypeFromValue(template?.timeout) === MultiTypeInputType.RUNTIME && (
+        <div className={cx(stepCss.formGroup, stepCss.sm)}>
+          <FormMultiTypeDurationField
+            multiTypeDurationProps={{
+              enableConfigureOptions: false,
+              allowableTypes,
+              expressions,
+              disabled: readonly
+            }}
+            label={getString('pipelineSteps.timeoutLabel')}
+            name={`${prefix}timeout`}
+            disabled={readonly}
+          />
+        </div>
+      )}
 
-        {getMultiTypeFromValue(template?.spec?.source?.spec?.script) === MultiTypeInputType.RUNTIME ? (
-          <div className={cx(stepCss.formGroup, stepCss.alignStart, stepCss.md)}>
-            <MultiTypeFieldSelector
+      {getMultiTypeFromValue(template?.spec?.source?.spec?.script) === MultiTypeInputType.RUNTIME ? (
+        <div className={cx(stepCss.formGroup, stepCss.alignStart, stepCss.md)}>
+          <MultiTypeFieldSelector
+            name={`${prefix}spec.source.spec.script`}
+            label={getString('script')}
+            defaultValueToReset=""
+            disabled={readonly}
+            allowedTypes={allowableTypes}
+            disableTypeSelection={readonly}
+            skipRenderValueInExpressionLabel
+            expressionRender={() => {
+              return (
+                <ShellScriptMonacoField
+                  name={`${prefix}spec.source.spec.script`}
+                  scriptType={scriptType}
+                  disabled={readonly}
+                  expressions={expressions}
+                />
+              )
+            }}
+          >
+            <ShellScriptMonacoField
               name={`${prefix}spec.source.spec.script`}
-              label={getString('script')}
-              defaultValueToReset=""
+              scriptType={scriptType}
               disabled={readonly}
-              allowedTypes={allowableTypes}
-              disableTypeSelection={readonly}
-              skipRenderValueInExpressionLabel
-              expressionRender={() => {
+              expressions={expressions}
+            />
+          </MultiTypeFieldSelector>
+        </div>
+      ) : null}
+
+      {isArray(template?.spec?.environmentVariables) && template?.spec?.environmentVariables ? (
+        <div className={stepCss.formGroup}>
+          <MultiTypeFieldSelector
+            name="spec.environmentVariables"
+            label={getString('pipeline.scriptInputVariables')}
+            defaultValueToReset={[]}
+            disableTypeSelection
+          >
+            <FieldArray
+              name="spec.environmentVariables"
+              render={() => {
                 return (
-                  <ShellScriptMonacoField
-                    name={`${prefix}spec.source.spec.script`}
-                    scriptType={scriptType}
-                    disabled={readonly}
-                    expressions={expressions}
-                  />
+                  <div className={css.panel}>
+                    <div className={css.environmentVarHeader}>
+                      <span className={css.label}>Name</span>
+                      <span className={css.label}>Type</span>
+                      <span className={css.label}>Value</span>
+                    </div>
+                    {template.spec.environmentVariables?.map((type, i: number) => {
+                      return (
+                        <div className={css.environmentVarHeader} key={type.value}>
+                          <FormInput.Text
+                            name={`${prefix}spec.environmentVariables[${i}].name`}
+                            placeholder={getString('name')}
+                            disabled={true}
+                          />
+                          <FormInput.Select
+                            items={scriptInputType}
+                            name={`${prefix}spec.environmentVariables[${i}].type`}
+                            placeholder={getString('typeLabel')}
+                            disabled={true}
+                          />
+                          <FormInput.MultiTextInput
+                            name={`${prefix}spec.environmentVariables[${i}].value`}
+                            multiTextInputProps={{
+                              allowableTypes,
+                              expressions,
+                              disabled: readonly
+                            }}
+                            label=""
+                            disabled={readonly}
+                            placeholder={getString('valueLabel')}
+                          />
+                        </div>
+                      )
+                    })}
+                  </div>
                 )
               }}
-            >
-              <ShellScriptMonacoField
-                name={`${prefix}spec.source.spec.script`}
-                scriptType={scriptType}
-                disabled={readonly}
-                expressions={expressions}
-              />
-            </MultiTypeFieldSelector>
-          </div>
-        ) : null}
-
-        {isArray(template?.spec?.environmentVariables) && template?.spec?.environmentVariables ? (
-          <div className={stepCss.formGroup}>
-            <MultiTypeFieldSelector
-              name="spec.environmentVariables"
-              label={getString('pipeline.scriptInputVariables')}
-              defaultValueToReset={[]}
-              disableTypeSelection
-            >
-              <FieldArray
-                name="spec.environmentVariables"
-                render={() => {
-                  return (
-                    <div className={css.panel}>
-                      <div className={css.environmentVarHeader}>
-                        <span className={css.label}>Name</span>
-                        <span className={css.label}>Type</span>
-                        <span className={css.label}>Value</span>
-                      </div>
-                      {template.spec.environmentVariables?.map((type, i: number) => {
-                        return (
-                          <div className={css.environmentVarHeader} key={type.value}>
-                            <FormInput.Text
-                              name={`${prefix}spec.environmentVariables[${i}].name`}
-                              placeholder={getString('name')}
-                              disabled={true}
-                            />
-                            <FormInput.Select
-                              items={scriptInputType}
-                              name={`${prefix}spec.environmentVariables[${i}].type`}
-                              placeholder={getString('typeLabel')}
-                              disabled={true}
-                            />
-                            <FormInput.MultiTextInput
-                              name={`${prefix}spec.environmentVariables[${i}].value`}
-                              multiTextInputProps={{
-                                allowableTypes,
-                                expressions,
-                                disabled: readonly
-                              }}
-                              label=""
-                              disabled={readonly}
-                              placeholder={getString('valueLabel')}
-                            />
-                          </div>
-                        )
-                      })}
-                    </div>
-                  )
-                }}
-              />
-            </MultiTypeFieldSelector>
-          </div>
-        ) : null}
-        {isArray(template?.spec?.outputVariables) && template?.spec?.outputVariables ? (
-          <div className={stepCss.formGroup}>
-            <MultiTypeFieldSelector
+            />
+          </MultiTypeFieldSelector>
+        </div>
+      ) : null}
+      {isArray(template?.spec?.outputVariables) && template?.spec?.outputVariables ? (
+        <div className={stepCss.formGroup}>
+          <MultiTypeFieldSelector
+            name="spec.outputVariables"
+            label={getString('pipeline.scriptOutputVariables')}
+            defaultValueToReset={[]}
+            disableTypeSelection
+          >
+            <FieldArray
               name="spec.outputVariables"
-              label={getString('pipeline.scriptOutputVariables')}
-              defaultValueToReset={[]}
-              disableTypeSelection
-            >
-              <FieldArray
-                name="spec.outputVariables"
-                render={() => {
-                  return (
-                    <div className={css.panel}>
-                      <div className={css.outputVarHeader}>
-                        <span className={css.label}>Name</span>
-                        <span className={css.label}>Type</span>
-                        <span className={css.label}>Value</span>
-                      </div>
-                      {template.spec.outputVariables?.map((output, i: number) => {
-                        return (
-                          <div className={css.outputVarHeader} key={output.name}>
-                            <FormInput.Text
-                              name={`${prefix}spec.outputVariables[${i}].name`}
-                              placeholder={getString('name')}
-                              disabled={true}
-                            />
-                            <FormInput.Select
-                              items={scriptOutputType}
-                              name={`${prefix}spec.outputVariables[${i}].type`}
-                              placeholder={getString('typeLabel')}
-                              disabled={true}
-                            />
-
-                            <FormInput.MultiTextInput
-                              name={`${prefix}spec.outputVariables[${i}].value`}
-                              multiTextInputProps={{
-                                allowableTypes,
-                                expressions,
-                                disabled: readonly
-                              }}
-                              label=""
-                              disabled={readonly}
-                              placeholder={getString('valueLabel')}
-                            />
-                          </div>
-                        )
-                      })}
+              render={() => {
+                return (
+                  <div className={css.panel}>
+                    <div className={css.outputVarHeader}>
+                      <span className={css.label}>Name</span>
+                      <span className={css.label}>Type</span>
+                      <span className={css.label}>Value</span>
                     </div>
-                  )
-                }}
-              />
-            </MultiTypeFieldSelector>
-          </div>
-        ) : null}
-        {getMultiTypeFromValue(template?.spec?.executionTarget?.host) === MultiTypeInputType.RUNTIME ? (
-          <div className={cx(stepCss.formGroup, stepCss.md)}>
-            <FormInput.MultiTextInput
-              placeholder={getString('cd.specifyTargetHost')}
-              label={getString('targetHost')}
-              multiTextInputProps={{
-                expressions,
-                disabled: readonly,
-                allowableTypes
-              }}
-              disabled={readonly}
-              name={`${prefix}spec.executionTarget.host`}
-            />
-          </div>
-        ) : null}
+                    {template.spec.outputVariables?.map((output, i: number) => {
+                      return (
+                        <div className={css.outputVarHeader} key={output.name}>
+                          <FormInput.Text
+                            name={`${prefix}spec.outputVariables[${i}].name`}
+                            placeholder={getString('name')}
+                            disabled={true}
+                          />
+                          <FormInput.Select
+                            items={scriptOutputType}
+                            name={`${prefix}spec.outputVariables[${i}].type`}
+                            placeholder={getString('typeLabel')}
+                            disabled={true}
+                          />
 
-        {getMultiTypeFromValue(template?.spec?.executionTarget?.connectorRef) === MultiTypeInputType.RUNTIME && (
-          <div className={cx(stepCss.formGroup, stepCss.md)}>
-            <MultiTypeSecretInput
-              type="SSHKey"
-              expressions={expressions}
-              allowableTypes={allowableTypes}
-              name={`${prefix}spec.executionTarget.connectorRef`}
-              label={getString('sshConnector')}
-              disabled={readonly}
-            />
-          </div>
-        )}
-
-        {getMultiTypeFromValue(template?.spec?.executionTarget?.workingDirectory) === MultiTypeInputType.RUNTIME ? (
-          <div className={cx(stepCss.formGroup, stepCss.md)}>
-            <FormInput.MultiTextInput
-              disabled={readonly}
-              placeholder={getString('cd.enterWorkDirectory')}
-              label={getString('workingDirectory')}
-              multiTextInputProps={{
-                expressions,
-                disabled: readonly,
-                allowableTypes
+                          <FormInput.MultiTextInput
+                            name={`${prefix}spec.outputVariables[${i}].value`}
+                            multiTextInputProps={{
+                              allowableTypes,
+                              expressions,
+                              disabled: readonly
+                            }}
+                            label=""
+                            disabled={readonly}
+                            placeholder={getString('valueLabel')}
+                          />
+                        </div>
+                      )
+                    })}
+                  </div>
+                )
               }}
-              name={`${prefix}spec.executionTarget.workingDirectory`}
             />
-          </div>
-        ) : null}
-      </FormikForm>
-    </Formik>
+          </MultiTypeFieldSelector>
+        </div>
+      ) : null}
+      {getMultiTypeFromValue(template?.spec?.executionTarget?.host) === MultiTypeInputType.RUNTIME ? (
+        <div className={cx(stepCss.formGroup, stepCss.md)}>
+          <FormInput.MultiTextInput
+            placeholder={getString('cd.specifyTargetHost')}
+            label={getString('targetHost')}
+            multiTextInputProps={{
+              expressions,
+              disabled: readonly,
+              allowableTypes
+            }}
+            disabled={readonly}
+            name={`${prefix}spec.executionTarget.host`}
+          />
+        </div>
+      ) : null}
+
+      {getMultiTypeFromValue(template?.spec?.executionTarget?.connectorRef) === MultiTypeInputType.RUNTIME && (
+        <div className={cx(stepCss.formGroup, stepCss.md)}>
+          <MultiTypeSecretInput
+            type="SSHKey"
+            expressions={expressions}
+            allowableTypes={allowableTypes}
+            name={`${prefix}spec.executionTarget.connectorRef`}
+            label={getString('sshConnector')}
+            disabled={readonly}
+          />
+        </div>
+      )}
+
+      {getMultiTypeFromValue(template?.spec?.executionTarget?.workingDirectory) === MultiTypeInputType.RUNTIME ? (
+        <div className={cx(stepCss.formGroup, stepCss.md)}>
+          <FormInput.MultiTextInput
+            disabled={readonly}
+            placeholder={getString('cd.enterWorkDirectory')}
+            label={getString('workingDirectory')}
+            multiTextInputProps={{
+              expressions,
+              disabled: readonly,
+              allowableTypes
+            }}
+            name={`${prefix}spec.executionTarget.workingDirectory`}
+          />
+        </div>
+      ) : null}
+    </FormikForm>
   )
 }
