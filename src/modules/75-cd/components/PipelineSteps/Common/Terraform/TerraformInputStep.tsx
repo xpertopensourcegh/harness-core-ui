c8960ff9dced257e4c75b7ac1c35270efbb87790
Ishant Mehta <43949150+ishantmehtaharness@users.noreply.github.com>
2022-05-17 23:58:05 +0530

    fix: [PIE-3776]: Fix Formik Wrap issue - Terraform (#8909)
    * fix: [PIE-3776]: Remove Formik wrapper from Terraform
    
    * fix: [PIE-3776]: Remove Formik wrapper from Terraform
    
    * fix: [PIE-3776]: Remove Formik wrapper from Terraform
    
    * test
    
    * test snapshot

--- src/modules/75-cd/components/PipelineSteps/Common/Terraform/TerraformInputStep.tsx
+++ src/modules/75-cd/components/PipelineSteps/Common/Terraform/TerraformInputStep.tsx
@@ -7,7 +7,7 @@
 
 import React from 'react'
 import cx from 'classnames'
-import { isEmpty, noop } from 'lodash-es'
+import { isEmpty } from 'lodash-es'
 
 import {
   getMultiTypeFromValue,
@@ -16,8 +16,7 @@ import {
   FormikForm,
   Text,
   Container,
-  Label,
-  Formik
+  Label
 } from '@wings-software/uicore'
 import { Color } from '@harness/design-system'
 import { useStrings } from 'framework/strings'
@@ -45,106 +44,102 @@ export default function TerraformInputStep<T extends TerraformData = TerraformDa
     onChange?.(arg as T)
   }
   return (
-    <Formik formName={'stepTestUtilForm'} initialValues={{}} onSubmit={noop}>
-      <FormikForm>
-        {getMultiTypeFromValue((inputSetData?.template as TerraformData)?.spec?.provisionerIdentifier) ===
-          MultiTypeInputType.RUNTIME && (
-          <div className={cx(stepCss.formGroup, stepCss.md)}>
-            <FormInput.MultiTextInput
-              name={`${path}.spec.provisionerIdentifier`}
-              placeholder={getString('pipeline.terraformStep.provisionerIdentifier')}
-              label={getString('pipelineSteps.provisionerIdentifier')}
-              disabled={readonly}
-              multiTextInputProps={{
-                expressions,
-                allowableTypes
-              }}
-            />
-          </div>
-        )}
-        {getMultiTypeFromValue(inputSetData?.template?.timeout) === MultiTypeInputType.RUNTIME && (
-          <div className={cx(stepCss.formGroup, stepCss.sm)}>
-            <FormMultiTypeDurationField
-              label={getString('pipelineSteps.timeoutLabel')}
-              name={`${isEmpty(inputSetData?.path) ? '' : `${inputSetData?.path}.`}timeout`}
-              disabled={readonly}
-              multiTypeDurationProps={{
-                enableConfigureOptions: false,
-                allowableTypes,
-                expressions,
-                disabled: readonly
-              }}
-            />
-          </div>
-        )}
-        <ConfigInputs {...props} onUpdate={onUpdateRef} onChange={onChangeRef} />
-        {inputSetData?.template?.spec?.configuration?.spec?.varFiles?.length && (
-          <Label style={{ color: Color.GREY_900, paddingBottom: 'var(--spacing-medium)' }}>
-            {getString('cd.terraformVarFiles')}
-          </Label>
-        )}
-        {inputSetData?.template?.spec?.configuration?.spec?.varFiles?.map((varFile: any, index) => {
-          if (varFile?.varFile?.type === TerraformStoreTypes.Inline) {
-            return (
-              <React.Fragment key={`${path}.spec.configuration.spec.varFiles[${index}]`}>
-                <Container flex width={120} padding={{ bottom: 'small' }}>
-                  <Text font={{ weight: 'bold' }}>{getString('cd.varFile')}:</Text>
-                  {varFile?.varFile?.identifier}
-                </Container>
+    <FormikForm>
+      {getMultiTypeFromValue((inputSetData?.template as TerraformData)?.spec?.provisionerIdentifier) ===
+        MultiTypeInputType.RUNTIME && (
+        <div className={cx(stepCss.formGroup, stepCss.md)}>
+          <FormInput.MultiTextInput
+            name={`${path}.spec.provisionerIdentifier`}
+            placeholder={getString('pipeline.terraformStep.provisionerIdentifier')}
+            label={getString('pipelineSteps.provisionerIdentifier')}
+            disabled={readonly}
+            multiTextInputProps={{
+              expressions,
+              allowableTypes
+            }}
+          />
+        </div>
+      )}
+      {getMultiTypeFromValue(inputSetData?.template?.timeout) === MultiTypeInputType.RUNTIME && (
+        <div className={cx(stepCss.formGroup, stepCss.sm)}>
+          <FormMultiTypeDurationField
+            label={getString('pipelineSteps.timeoutLabel')}
+            name={`${isEmpty(inputSetData?.path) ? '' : `${inputSetData?.path}.`}timeout`}
+            disabled={readonly}
+            multiTypeDurationProps={{
+              enableConfigureOptions: false,
+              allowableTypes,
+              expressions,
+              disabled: readonly
+            }}
+          />
+        </div>
+      )}
+      <ConfigInputs {...props} onUpdate={onUpdateRef} onChange={onChangeRef} />
+      {inputSetData?.template?.spec?.configuration?.spec?.varFiles?.length && (
+        <Label style={{ color: Color.GREY_900, paddingBottom: 'var(--spacing-medium)' }}>
+          {getString('cd.terraformVarFiles')}
+        </Label>
+      )}
+      {inputSetData?.template?.spec?.configuration?.spec?.varFiles?.map((varFile: any, index) => {
+        if (varFile?.varFile?.type === TerraformStoreTypes.Inline) {
+          return (
+            <React.Fragment key={`${path}.spec.configuration.spec.varFiles[${index}]`}>
+              <Container flex width={120} padding={{ bottom: 'small' }}>
+                <Text font={{ weight: 'bold' }}>{getString('cd.varFile')}:</Text>
+                {varFile?.varFile?.identifier}
+              </Container>
 
-                {getMultiTypeFromValue(varFile?.varFile?.spec?.content) === MultiTypeInputType.RUNTIME && (
-                  <div className={cx(stepCss.formGroup, stepCss.md)}>
-                    <FormInput.MultiTextInput
-                      name={`${path}.spec.configuration.spec.varFiles[${index}].varFile.spec.content`}
-                      label={getString('pipelineSteps.content')}
-                      multiTextInputProps={{
-                        expressions,
-                        allowableTypes
-                      }}
-                    />
-                  </div>
-                )}
-              </React.Fragment>
-            )
-          } else if (varFile.varFile?.type === TerraformStoreTypes.Remote) {
-            return (
-              <TFRemoteSection
-                remoteVar={varFile}
-                index={index}
-                {...props}
-                onUpdate={onUpdateRef}
-                onChange={onChangeRef}
-              />
-            )
-          }
-          return <></>
-        })}
+              {getMultiTypeFromValue(varFile?.varFile?.spec?.content) === MultiTypeInputType.RUNTIME && (
+                <div className={cx(stepCss.formGroup, stepCss.md)}>
+                  <FormInput.MultiTextInput
+                    name={`${path}.spec.configuration.spec.varFiles[${index}].varFile.spec.content`}
+                    label={getString('pipelineSteps.content')}
+                    multiTextInputProps={{
+                      expressions,
+                      allowableTypes
+                    }}
+                  />
+                </div>
+              )}
+            </React.Fragment>
+          )
+        } else if (varFile.varFile?.type === TerraformStoreTypes.Remote) {
+          return (
+            <TFRemoteSection
+              remoteVar={varFile}
+              index={index}
+              {...props}
+              onUpdate={onUpdateRef}
+              onChange={onChangeRef}
+            />
+          )
+        }
+        return <></>
+      })}
 
-        {getMultiTypeFromValue(inputSetData?.template?.spec?.configuration?.spec?.backendConfig?.spec?.content) ===
-          MultiTypeInputType.RUNTIME && (
-          <div className={cx(stepCss.formGroup, stepCss.md)}>
-            <FormInput.TextArea
-              name={`${path}.spec.configuration.spec.backendConfig.spec.content`}
-              label={getString('cd.backEndConfig')}
-            />
-          </div>
-        )}
-        {getMultiTypeFromValue(inputSetData?.template?.spec?.configuration?.spec?.targets as string) ===
-          MultiTypeInputType.RUNTIME && (
-          <div className={cx(stepCss.formGroup, stepCss.md)}>
-            <List
-              name={`${path}.spec.configuration.spec.targets`}
-              label={
-                <Text style={{ display: 'flex', alignItems: 'center' }}>{getString('pipeline.targets.title')}</Text>
-              }
-              disabled={readonly}
-              style={{ marginBottom: 'var(--spacing-small)' }}
-              expressions={expressions}
-              isNameOfArrayType
-            />
-          </div>
-        )}
-      </FormikForm>
-    </Formik>
+      {getMultiTypeFromValue(inputSetData?.template?.spec?.configuration?.spec?.backendConfig?.spec?.content) ===
+        MultiTypeInputType.RUNTIME && (
+        <div className={cx(stepCss.formGroup, stepCss.md)}>
+          <FormInput.TextArea
+            name={`${path}.spec.configuration.spec.backendConfig.spec.content`}
+            label={getString('cd.backEndConfig')}
+          />
+        </div>
+      )}
+      {getMultiTypeFromValue(inputSetData?.template?.spec?.configuration?.spec?.targets as string) ===
+        MultiTypeInputType.RUNTIME && (
+        <div className={cx(stepCss.formGroup, stepCss.md)}>
+          <List
+            name={`${path}.spec.configuration.spec.targets`}
+            label={<Text style={{ display: 'flex', alignItems: 'center' }}>{getString('pipeline.targets.title')}</Text>}
+            disabled={readonly}
+            style={{ marginBottom: 'var(--spacing-small)' }}
+            expressions={expressions}
+            isNameOfArrayType
+          />
+        </div>
+      )}
+    </FormikForm>
   )
 }
