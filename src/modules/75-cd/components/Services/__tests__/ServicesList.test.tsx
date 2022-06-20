d93d61437f18469e39718b8e8bbe0ed7c5f20d17
Sagar-Rajput7 <sagar.rajput@harness.io>
2022-06-20 10:11:42 +0530

    feat: [PIE-4089]: added test cases for servicesList (#9519)

--- src/modules/75-cd/components/Services/__tests__/ServicesList.test.tsx
+++ src/modules/75-cd/components/Services/__tests__/ServicesList.test.tsx
@@ -6,33 +6,40 @@
  */
 
 import React from 'react'
-import { render } from '@testing-library/react'
+import { fireEvent, render, waitFor } from '@testing-library/react'
 import { noop } from 'lodash-es'
-import { TestWrapper } from '@common/utils/testUtils'
+import { findDialogContainer, TestWrapper } from '@common/utils/testUtils'
 import { ServicesList } from '@cd/components/Services/ServicesList/ServicesList'
 import { serviceDetails } from '@cd/mock'
 import type { ServiceDetailsDTO } from 'services/cd-ng'
+import mockImport from 'framework/utils/mockImport'
 
 jest.mock('highcharts-react-official', () => () => <></>)
 
-describe('ServicesList', () => {
-  test('render', () => {
-    const { container } = render(
-      <TestWrapper
-        path="account/:accountId/cd/orgs/:orgIdentifier/projects/:projectIdentifier/services"
-        pathParams={{ accountId: 'dummy', orgIdentifier: 'dummy', projectIdentifier: 'dummy' }}
-      >
-        <ServicesList
-          loading={false}
-          error={false}
-          data={serviceDetails.data.serviceDeploymentDetailsList as unknown as ServiceDetailsDTO[]}
-          refetch={noop}
-        />
-      </TestWrapper>
-    )
-    expect(container).toMatchSnapshot()
+jest.mock('services/cd-ng', () => {
+  return {
+    useGetYamlSchema: jest.fn().mockImplementation(() => ({ refetch: jest.fn(), loading: false })),
+    useDeleteServiceV2: jest.fn(() => ({ mutate: jest.fn() })),
+    useCreateServiceV2: jest.fn().mockImplementation(() => ({ refetch: jest.fn(), loading: false, data: {} })),
+    useUpsertServiceV2: jest.fn().mockImplementation(() => ({
+      cancel: jest.fn(),
+      loading: false,
+      mutate: jest.fn().mockImplementation(() => {
+        return {
+          status: 'SUCCESS'
+        }
+      })
+    }))
+  }
+})
+const mutate = jest.fn(() => {
+  return Promise.resolve({
+    status: 'SUCCESS',
+    data: {}
   })
 })
+
+export const findDialogMenu = (): HTMLElement | null => document.querySelector('.bp3-menu')
 
 const renderSetup = (data: ServiceDetailsDTO[]) => {
   return render(
@@ -44,6 +51,123 @@ const renderSetup = (data: ServiceDetailsDTO[]) => {
     </TestWrapper>
   )
 }
+
+describe('ServicesList', () => {
+  test('render', () => {
+    const responseData = serviceDetails.data.serviceDeploymentDetailsList as unknown as ServiceDetailsDTO[]
+    const { container } = renderSetup(responseData)
+    expect(container).toMatchSnapshot()
+  })
+
+  test('open option menu and "open in new tab"', async () => {
+    const responseData = serviceDetails.data.serviceDeploymentDetailsList as unknown as ServiceDetailsDTO[]
+    const { container } = renderSetup(responseData)
+    fireEvent.click(container.querySelector('[data-icon="Options"]') as HTMLElement)
+    const menuDailog = findDialogMenu()
+    expect(menuDailog).toBeTruthy()
+
+    const launchBtn = menuDailog!.querySelector('[data-icon="launch"]')
+    expect(launchBtn).toBeTruthy()
+    fireEvent.click(launchBtn!)
+
+    expect(launchBtn?.closest('a')).toHaveAttribute(
+      'href',
+      '/account/dummy/undefined/orgs/dummy/projects/dummy/services/asdfasdf'
+    )
+  })
+
+  test('should open edit modal and cancel', async () => {
+    const responseData = serviceDetails.data.serviceDeploymentDetailsList as unknown as ServiceDetailsDTO[]
+    const { container, getByText } = renderSetup(responseData)
+
+    fireEvent.click(container.querySelector('[data-icon="Options"]') as HTMLElement)
+    const menuDailog = findDialogMenu()
+    expect(menuDailog).toBeTruthy()
+
+    const editBtn = menuDailog!.querySelector('[data-icon="edit"]')
+    fireEvent.click(editBtn!)
+
+    const form = findDialogContainer()
+    expect(form).toBeTruthy()
+
+    expect(getByText('cancel')).toBeDefined()
+    fireEvent.click(getByText('cancel') as HTMLButtonElement)
+    expect(findDialogContainer()).toBeFalsy()
+  })
+
+  test('Should allow deleting', async () => {
+    mockImport('services/cd-ng', {
+      useDeleteServiceV2: () => ({
+        mutate
+      })
+    })
+
+    const responseData = serviceDetails.data.serviceDeploymentDetailsList as unknown as ServiceDetailsDTO[]
+    const { container, getByText } = renderSetup(responseData)
+
+    fireEvent.click(container.querySelector('[data-icon="Options"]') as HTMLElement)
+    const menuDailog = findDialogMenu()
+    expect(menuDailog).toBeTruthy()
+
+    fireEvent.click(menuDailog!.querySelector('[icon="trash"]') as HTMLElement)
+    let form = findDialogContainer()
+    expect(form).toBeTruthy()
+    expect(getByText('confirm')).toBeDefined()
+    fireEvent.click(getByText('confirm') as HTMLButtonElement)
+    await waitFor(() => expect(mutate).toBeCalledTimes(1))
+
+    fireEvent.click(container.querySelector('[data-icon="Options"]') as HTMLElement)
+    fireEvent.click(menuDailog!.querySelector('[icon="trash"]') as HTMLElement)
+    form = findDialogContainer()
+    expect(form).toBeTruthy()
+    expect(getByText('cancel')).toBeDefined()
+    fireEvent.click(getByText('cancel') as HTMLButtonElement)
+    expect(findDialogContainer()).toBeFalsy()
+  })
+
+  test('Delete with error', async () => {
+    mockImport('services/cd-ng', {
+      useDeleteServiceV2: jest.fn().mockImplementation(() => {
+        return {
+          mutate: jest.fn().mockRejectedValue({
+            data: {
+              message: 'Deletion failed'
+            }
+          })
+        }
+      })
+    })
+
+    const responseData = serviceDetails.data.serviceDeploymentDetailsList as unknown as ServiceDetailsDTO[]
+    const { container, getByText } = renderSetup(responseData)
+
+    fireEvent.click(container.querySelector('[data-icon="Options"]') as HTMLElement)
+    const menuDailog = findDialogMenu()
+    expect(menuDailog).toBeTruthy()
+
+    fireEvent.click(menuDailog!.querySelector('[icon="trash"]') as HTMLElement)
+    const form = findDialogContainer()
+    expect(form).toBeTruthy()
+    expect(getByText('confirm')).toBeDefined()
+    fireEvent.click(getByText('confirm') as HTMLButtonElement)
+    await waitFor(() => expect(mutate).toBeCalledTimes(1))
+
+    expect(findDialogContainer()).toBeFalsy()
+    await waitFor(() => expect(getByText('Deletion failed')).toBeDefined())
+  })
+
+  test('go to service details on row click', async () => {
+    const responseData = serviceDetails.data.serviceDeploymentDetailsList as unknown as ServiceDetailsDTO[]
+    const { container, getByTestId } = renderSetup(responseData)
+    const row = container.getElementsByClassName('TableV2--row TableV2--card TableV2--clickable')[0]
+    await fireEvent.click(row!)
+    await waitFor(() => getByTestId('location'))
+
+    expect(getByTestId('location')).toHaveTextContent(
+      '/account/dummy/undefined/orgs/dummy/projects/dummy/services/asdfasdf'
+    )
+  })
+})
 
 describe('DeploymentType in ServicesList ', () => {
   test('render Kubernetes deployment type', () => {
