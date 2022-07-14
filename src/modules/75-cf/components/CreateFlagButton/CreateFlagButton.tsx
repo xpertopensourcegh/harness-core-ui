3d04574055a785799d2882c5234f661d2f410dd9
laura-mannus <103498167+laura-mannus@users.noreply.github.com>
2022-07-14 14:15:24 +0100

    fix: [FFM-3552]: Updating + from string to icon, adjusting padding on New Flag Button (#9996)
    * fix: [FFM-3552]: Updating + from string to icon removing unnecessary styling

--- src/modules/75-cf/components/CreateFlagButton/CreateFlagButton.tsx
+++ src/modules/75-cf/components/CreateFlagButton/CreateFlagButton.tsx
@@ -6,7 +6,7 @@
  */
 
 import React, { ReactElement } from 'react'
-import { ButtonVariation } from '@wings-software/uicore'
+import { ButtonVariation } from '@harness/uicore'
 import RbacButton from '@rbac/components/Button/Button'
 import { ResourceType } from '@rbac/interfaces/ResourceType'
 import { PermissionIdentifier } from '@rbac/interfaces/PermissionIdentifier'
@@ -15,7 +15,6 @@ import { FeatureIdentifier } from 'framework/featureStore/FeatureIdentifier'
 import { useStrings } from 'framework/strings'
 import { useTelemetry } from '@common/hooks/useTelemetry'
 import { FeatureActions, Category } from '@common/constants/TrackingConstants'
-import css from './CreateFlagButton.module.scss'
 
 export interface CreateFlagButtonProps {
   disabled?: boolean
@@ -43,6 +42,7 @@ const CreateFlagButton = (props: CreateFlagButtonProps): ReactElement => {
       data-testid="create-flag-button"
       disabled={disabled}
       text={getString('cf.featureFlags.newFlag')}
+      icon="plus"
       intent="primary"
       variation={ButtonVariation.PRIMARY}
       onClick={() => {
@@ -51,7 +51,6 @@ const CreateFlagButton = (props: CreateFlagButtonProps): ReactElement => {
         })
         showModal()
       }}
-      className={css.openModalBtn}
       permission={{
         permission: PermissionIdentifier.EDIT_FF_FEATUREFLAG,
         resource: { resourceType: ResourceType.FEATUREFLAG }
