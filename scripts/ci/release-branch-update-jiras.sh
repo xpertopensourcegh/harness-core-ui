057a231e4c03af48cdc5f0d9b1db77c79d7fa89b
diptiman.adak <diptiman.adak@harness.io>
2022-08-01 16:09:04 +0530

    Revert "[fix]: [BT-1623]: Fixing the JIRA project (#10319)"
    This reverts commit 6ba94f2d2793e6ba6467a0d646d9081796a9f379.

--- scripts/ci/release-branch-update-jiras.sh
+++ scripts/ci/release-branch-update-jiras.sh
@@ -10,10 +10,8 @@ then
   exit 1
 fi
 VERSION=$(cat package.json | grep version | cut -d: -f2 | cut -d\" -f2)
-echo "version: $VERSION"
-echo "projects: $PROJECTS"
-KEYS=`git log --pretty=oneline --abbrev-commit | awk "1;/Branching to release\//{exit}" | grep -o -iE '(BT|CCE|CCM|CDS|CE|CI|COMP|CV|CVNG|DEL|DOC|DX|ER|FFM|OPS|OPA|PIE|PL|SEC|STO|SWAT|GTM|ONP|CHAOS)-[0-9]+' | sort | uniq`
-#KEYS=`git log --pretty=oneline --abbrev-commit | awk "1;/Branching to release\//{exit}" | grep -o -iE '('${PROJECTS}')-[0-9]+' | sort | uniq`
+echo $VERSION
+KEYS=`git log --pretty=oneline --abbrev-commit | awk "1;/Branching to release\//{exit}" | grep -o -iE '('${PROJECTS}')-[0-9]+' | sort | uniq`
 echo --- List of JIRA ---
 echo $KEYS
 echo --- End
