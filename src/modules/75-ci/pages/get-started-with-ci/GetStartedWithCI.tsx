467bf65b4ac2639eba787d5db702592bd2ed56e8
Vardan Bansal <vardan.bansal@harness.io>
2022-08-06 02:01:47 +0530

    fix: Fix minor flickering issue for Provisioning wizard (#10441)

--- src/modules/75-ci/pages/get-started-with-ci/GetStartedWithCI.tsx
+++ src/modules/75-ci/pages/get-started-with-ci/GetStartedWithCI.tsx
@@ -209,7 +209,7 @@ export default function GetStartedWithCI(): React.ReactElement {
   return (
     <>
       {showPageLoader ? <PageSpinner /> : <></>}
-      {!showPageLoader && showWizard ? (
+      {showWizard ? (
         <InfraProvisioningWizard
           precursorData={{
             preSelectedGitConnector,
