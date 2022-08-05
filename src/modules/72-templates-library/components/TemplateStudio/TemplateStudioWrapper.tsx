db99ec85e4988a546822fdd0ed5a9899621c6295
Yogesh Chaudhary <81607640+yc147@users.noreply.github.com>
2022-08-05 20:02:52 +0530

    fix: [CDS-41193]: stage selection for account/org template (#10435)
    * fix: [CDS-41193]: stage selection for account/org template
    
    * fix: [CDS-41193]: stage selection for account/org template

--- src/modules/72-templates-library/components/TemplateStudio/TemplateStudioWrapper.tsx
+++ src/modules/72-templates-library/components/TemplateStudio/TemplateStudioWrapper.tsx
@@ -7,6 +7,7 @@
 
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import { defaultTo } from 'lodash-es'
 import { TemplateStudio } from '@templates-library/components/TemplateStudio/TemplateStudio'
 import { TemplateProvider } from '@templates-library/components/TemplateStudio/TemplateContext/TemplateContext'
 import type {
@@ -18,6 +19,12 @@ import type {
 import { useQueryParams } from '@common/hooks'
 import { GitSyncStoreProvider } from 'framework/GitRepoStore/GitSyncStoreContext'
 import type { PipelineContextInterface } from '@pipeline/components/PipelineStudio/PipelineContext/PipelineContext'
+import { useFeatureFlags } from '@common/hooks/useFeatureFlag'
+import { StageType } from '@pipeline/utils/stageHelpers'
+import { PipelineStages, PipelineStagesProps } from '@pipeline/components/PipelineStages/PipelineStages'
+import { stagesCollection } from '@pipeline/components/PipelineStudio/Stages/StagesCollection'
+import { useLicenseStore } from 'framework/LicenseStore/LicenseStoreContext'
+import { useStrings } from 'framework/strings'
 
 interface TemplateStudioWrapperProps {
   renderPipelineStage?: PipelineContextInterface['renderPipelineStage']
@@ -28,13 +35,32 @@ export const TemplateStudioWrapper: (props: TemplateStudioWrapperProps) => JSX.E
     TemplateStudioPathProps & ModulePathParams
   >()
   const { versionLabel, repoIdentifier, branch } = useQueryParams<TemplateStudioQueryParams & GitQueryParams>()
+  const { licenseInformation } = useLicenseStore()
+  const { CING_ENABLED, CDNG_ENABLED, CFNG_ENABLED, SECURITY_STAGE } = useFeatureFlags()
+  const { getString } = useStrings()
+
+  const defaultRenderPipelineStage = (args: Omit<PipelineStagesProps, 'children'>) => {
+    return (
+      <PipelineStages {...args}>
+        {stagesCollection.getStage(StageType.DEPLOY, !!CDNG_ENABLED && !!licenseInformation['CD'], getString)}
+        {stagesCollection.getStage(StageType.BUILD, !!CING_ENABLED && !!licenseInformation['CI'], getString)}
+        {stagesCollection.getStage(StageType.APPROVAL, true, getString)}
+        {stagesCollection.getStage(StageType.FEATURE, !!CFNG_ENABLED && !!licenseInformation['CF'], getString)}
+        {stagesCollection.getStage(StageType.SECURITY, !!SECURITY_STAGE, getString)}
+        {/*{stagesCollection.getStage(StageType.PIPELINE, false, getString)}*/}
+        {stagesCollection.getStage(StageType.CUSTOM, true, getString)}
+        {stagesCollection.getStage(StageType.Template, false, getString)}
+      </PipelineStages>
+    )
+  }
+
   return (
     <TemplateProvider
       queryParams={{ accountIdentifier: accountId, orgIdentifier, projectIdentifier, repoIdentifier, branch }}
       templateIdentifier={templateIdentifier}
       versionLabel={versionLabel}
       templateType={templateType}
-      renderPipelineStage={renderPipelineStage}
+      renderPipelineStage={defaultTo(renderPipelineStage, defaultRenderPipelineStage)}
     >
       <GitSyncStoreProvider>
         <TemplateStudio />
